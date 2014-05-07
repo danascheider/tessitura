baebdb74845d2574b68e5958687c9ff9958fd8a3
danascheider <dana.scheider@gmail.com>
2014-05-07 14:18:37 -0700

    Add foreign keys for Categorizations table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-  has_many :categorizations
+  has_many :categorizations, foreign_key: :category_id
   has_many :todo_items, through: :categorizations
 end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,5 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :user
-  has_many :categorizations
+  has_many :categorizations, foreign_key: :todo_id
   has_many :categories, through: :categorizations
 end
