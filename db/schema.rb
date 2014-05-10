11e1da7ea34b9d513a732ea64cbe4f3ac81c0b41
danascheider <dana.scheider@gmail.com>
2014-05-10 14:29:12 -0700

    Create users, tweak DB relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507202948) do
+ActiveRecord::Schema.define(version: 20140510212500) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140507202948) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
