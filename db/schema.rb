700b75e3893ebdc771b2a1a866bd4a93ad10e7d4
danascheider <dana.scheider@gmail.com>
2014-05-16 14:37:37 -0700

    Run migration adding username and password to User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510212500) do
+ActiveRecord::Schema.define(version: 20140516213049) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20140510212500) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
+    t.string   "password"
   end
 
 end
