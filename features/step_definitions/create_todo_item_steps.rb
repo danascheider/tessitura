e636da2551d638a6714d8d9a027e119669017f95
danascheider <dana.scheider@gmail.com>
2014-05-16 18:03:19 -0700

    Define 'submit the filled-out form' step

--- features/step_definitions/create_todo_item_steps.rb
+++ features/step_definitions/create_todo_item_steps.rb
@@ -5,3 +5,11 @@ end
 When /^I navigate to my new to-do item page$/ do
   visit new_user_todo_item_path(@user.id)
 end
+
+When /^I submit the filled-out form$/ do 
+  fill_in('Title', {with: "My To-Do Item"})
+  fill_in('Description', {with: "Test to-do items with Cucumber & Capybara"})
+  choose('todo_item_priority_urgent')
+  select('Blocking', {from: 'todo_item_status'})
+  click_button('Create Todo item')
+end
