e2b5602c58439ea23939af39933503ab3aa5120a
danascheider <dana.scheider@gmail.com>
2014-05-16 17:40:22 -0700

    Add defininitions for first 2 steps

--- features/step_definitions/create_todo_item_steps.rb
+++ features/step_definitions/create_todo_item_steps.rb
@@ -1,3 +1,7 @@
-Given /^I am logged in$/ do
-  true # Login not implemented - pass test first
+Given /^I am a user$/ do
+  @user = FactoryGirl.create(:user)
+end
+
+When /^I navigate to my new to-do item page$/ do
+  visit new_user_todo_item_path(@user.id)
 end
