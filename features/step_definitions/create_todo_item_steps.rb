8da52b1fa089b9ed6f1c716bc77c9fcf3546a12b
danascheider <dana.scheider@gmail.com>
2014-05-20 13:15:30 -0700

    Add test to check values of TodoItem attributes on creation

--- features/step_definitions/create_todo_item_steps.rb
+++ features/step_definitions/create_todo_item_steps.rb
@@ -2,6 +2,9 @@ When /^I navigate to my new to-do item page$/ do
   visit new_user_todo_item_path(@user.id)
 end
 
+### Test form submission & validation
+
+### These two tests go together and need refactoring so they can be separated ###
 When /^I submit the filled-out form$/ do 
   fill_in('Title', {with: "My To-Do Item"})
   fill_in('Description', {with: "Test to-do items with Cucumber & Capybara"})
@@ -9,6 +12,15 @@ When /^I submit the filled-out form$/ do
   select('Blocking', {from: 'todo_item_status'})
   click_button('Create Todo item')
 end
+
+Then /my last to-do item should have the information from the form$/ do 
+  todo_item = TodoItem.where(user: @user).last
+  expect(todo_item.title).to eql "My To-Do Item"
+  expect(todo_item.description).to eql("Test to-do items with Cucumber & Capybara")
+  expect(todo_item.priority).to eql('Urgent')
+  expect(todo_item.status).to eql('Blocking')
+end
+### End grouped tests ###
 
 When /^I submit the form with no title$/ do 
   fill_in('Description', {with: "Test to-do items with Cucumber & Capybara"})
