0c74f83b00f9fd9c32e956ca2761f9e9d6ce18a2
danascheider <dana.scheider@gmail.com>
2014-05-21 10:51:43 -0700

    Define ensure_status method to set nil status to 'New' on save

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,7 +1,12 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :user
+  before_save :ensure_status
   has_many :categorizations, foreign_key: :todo_id
   has_many :categories, through: :categorizations
   validates :title, presence: true
   scope :incomplete, -> { where.not(status: 'Complete')}
+
+  def ensure_status
+    self.status ||= 'New'
+  end
 end
