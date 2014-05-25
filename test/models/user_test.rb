e79550c298f4eb52d2f7cdc2e58d71a3b57d4142
danascheider <dana.scheider@gmail.com>
2014-05-24 20:22:41 -0700

    Incorporate factories

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "invalid without a username" do 
-    u = User.new 
+    u = Factory(:user)
     assert !u.valid?, "User is valid without a username"
   end
 
   test "has unique username" do 
-    u = User.new(username: 'frank')
-    v = User.new(username: 'frank')
+    u = Factory(:user)
+    v = Factory(:user)
     assert !v.valid?, "User is valid without unique username"
   end
 end
