2e231763765ebbbf6a2947e59b168c18a66b29a1
danascheider <dana.scheider@gmail.com>
2014-05-25 13:25:00 -0700

    Add association :user

--- test/factories/todo_item.rb
+++ test/factories/todo_item.rb
@@ -1,9 +1,7 @@
 FactoryGirl.define do 
   factory :todo_item do 
+    association :user
     sequence(:title) {|i| "To-do Item #{i}"}
     status 'Blocking'
-    priority 'High'
-    deadline Time.now
-    description 'Figure out how to make Canto work so we can release v. 0.1.0!'
   end
 end
