55ac74b39ce276b86d6914b11cf003d5f62dae3b
danascheider <dana.scheider@gmail.com>
2014-05-24 19:59:21 -0700

    Get rid of fixtures, start writing user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid without a username" do 
+    u = User.new 
+    assert !u.valid?
+  end
 end
