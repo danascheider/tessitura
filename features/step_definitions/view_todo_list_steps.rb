09e0817b1d38d56928429a5b0b1b3a12b1e6e0ae
danascheider <dana.scheider@gmail.com>
2014-05-25 12:43:22 -0700

    Add pending to non-passing steps; refactor step 'I have x to-do items' for brevity, better use of factories

--- features/step_definitions/view_todo_list_steps.rb
+++ features/step_definitions/view_todo_list_steps.rb
@@ -1,15 +1,10 @@
 ### Scenario 1: To-do list is empty ###
 
 Given /^I have (no|\d+) to\-do items$/ do |number|
-  if number == 'no' || number == 0
+  if number == 'no' || (number = number.to_i) == 0
     @user.todo_items.length == 0
   else
-    number = number.to_i
-    i = 1
-    number.times do 
-      FactoryGirl.create(:todo_item, title: "My Task #{i}", user_id: @user.id)
-      i += 1
-    end
+    number.times { FactoryGirl.create(:todo_item, user_id: @user.id) }
   end
   @todo_list = @user.todo_items.to_a
 end
@@ -37,13 +32,14 @@ Then(/^I should see all of my to\-do items$/) do
 end
 
 Then(/^I should not see anyone else\'s to\-do items$/) do 
+  pending
   page.should have_css(".todo-list-item", count: @todo_list.length )
 end
 
 ### Scenario 3: Some tasks are complete
 Given /^(\d+) of them have been marked (.*)$/ do |number, status|
-  number = number.to_i
-  for i in 0..(number - 1)
+  pending
+  for i in 0..(number.to_i - 1)
     @todo_list[i].status = 'Complete'
   end
 end
