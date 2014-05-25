3c01191d7525a52269312eddabff24106dd87e2e
danascheider <dana.scheider@gmail.com>
2014-05-24 20:00:32 -0700

    Add test 'invalid without a username'

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test "invalid without a username" do 
     u = User.new 
-    assert !u.valid?
+    assert !u.valid?, "User is valid without a username"
   end
 end
