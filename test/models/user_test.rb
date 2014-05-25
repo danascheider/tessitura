9bc21a796a26ec7e21cdbd797f061779cf94f283
danascheider <dana.scheider@gmail.com>
2014-05-24 20:04:35 -0700

    Add test for unique username

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,10 @@ class UserTest < ActiveSupport::TestCase
     u = User.new 
     assert !u.valid?, "User is valid without a username"
   end
+
+  test "has unique username" do 
+    u = User.new(username: 'frank')
+    v = User.new(username: 'frank')
+    assert !v.valid?, "User is valid without unique username"
+  end
 end
