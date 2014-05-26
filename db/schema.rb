8148bd6467a7e1cb8ea501b0f238ef855a33af46
danascheider <dana.scheider@gmail.com>
2014-05-26 13:37:13 -0700

    Add e-mail to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524001937) do
+ActiveRecord::Schema.define(version: 20140526203527) do
 
   create_table "todo_items", force: true do |t|
     t.string   "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140524001937) do
     t.string   "password"
     t.boolean  "logged_in"
     t.boolean  "is_admin"
+    t.string   "email"
   end
 
 end
