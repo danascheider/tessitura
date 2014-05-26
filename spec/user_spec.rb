ee94a735007ad794e56b62280267f2e473ed9880
danascheider <dana.scheider@gmail.com>
2014-05-26 15:26:08 -0700

    describe 'validating user' - require password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,19 @@
 require "spec_helper"
 
 describe User do 
-  it "is invalid without a username" do 
-    user = User.new
-    expect(user).not_to be_valid
-  end
+  describe 'validating a user' do 
+    before do 
+      @user = User.new
+    end
 
-  it "is invalid without a password" do 
-    user = FactoryGirl.create(:user, password: nil)
-    expect(user).not_to be_valid
+    it "is invalid without a username" do 
+      @user.password = 'mypasswd1'
+      expect(@user).not_to be_valid
+    end
+
+    it "is invalid without a password" do 
+      @user.username = 'frank'
+      expect(@user).not_to be_valid
+    end
   end
 end
