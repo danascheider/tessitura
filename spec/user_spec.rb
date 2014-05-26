2f9ea5a0224a44728cb2aa0180fa2ef0cfcbfd0f
danascheider <dana.scheider@gmail.com>
2014-05-26 15:29:36 -0700

    Add user info to tests, define 'is invalid without an e-mail'

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,11 +8,19 @@ describe User do
 
     it "is invalid without a username" do 
       @user.password = 'mypasswd1'
+      @user.email = 'frank@example.com'
       expect(@user).not_to be_valid
     end
 
     it "is invalid without a password" do 
       @user.username = 'frank'
+      @user.email = 'frank@example.com'
+      expect(@user).not_to be_valid
+    end
+
+    it "is invalid without an e-mail" do 
+      @user.username = 'frank'
+      @user.password = 'mypasswd1'
       expect(@user).not_to be_valid
     end
   end
