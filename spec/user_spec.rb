a4e0306b21a4c2b83483cf483e3641ddfac03212
danascheider <dana.scheider@gmail.com>
2014-05-26 15:32:01 -0700

    Refactor to put user info in before block

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,24 +3,21 @@ require "spec_helper"
 describe User do 
   describe 'validating a user' do 
     before do 
-      @user = User.new
+      @user = User.new(username: 'frank', password: 'mypasswd1', email: 'frank@example.com')
     end
 
     it "is invalid without a username" do 
-      @user.password = 'mypasswd1'
-      @user.email = 'frank@example.com'
+      @user.username = nil
       expect(@user).not_to be_valid
     end
 
     it "is invalid without a password" do 
-      @user.username = 'frank'
-      @user.email = 'frank@example.com'
+      @user.password = nil
       expect(@user).not_to be_valid
     end
 
     it "is invalid without an e-mail" do 
-      @user.username = 'frank'
-      @user.password = 'mypasswd1'
+      @user.email = nil
       expect(@user).not_to be_valid
     end
   end
