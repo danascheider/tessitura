50caa1a189c7fc1b93aa344a0c27e33a17d041b7
danascheider <dana.scheider@gmail.com>
2014-05-26 15:42:36 -0700

    Validate uniqueness of e-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :todo_items, dependent: :destroy, foreign_key: :user_id
   validates_associated :todo_items
   validates :username, :password, :email, presence: true
+  validates :username, uniqueness: true
 end
