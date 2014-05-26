3bf429c5c197e36eef9eade45a92a0f09cbf8826
danascheider <dana.scheider@gmail.com>
2014-05-26 15:39:30 -0700

    Combine validations into one line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   has_many :todo_items, dependent: :destroy, foreign_key: :user_id
   validates_associated :todo_items
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :email, presence: true
+  validates :username, :password, :email, presence: true
 end
