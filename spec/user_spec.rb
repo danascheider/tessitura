cb5554b196f70dcc70b958edd963da2ec45245f1
danascheider <dana.scheider@gmail.com>
2014-05-26 15:20:56 -0700

    Write (failing) test 'is invalid without a password'

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,4 +5,9 @@ describe User do
     user = User.new
     expect(user).not_to be_valid
   end
+
+  it "is invalid without a password" do 
+    user = FactoryGirl.create(:user, password: nil)
+    expect(user).not_to be_valid
+  end
 end
