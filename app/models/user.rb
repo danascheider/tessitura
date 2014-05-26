76dc8a1f3474f455a260bed37f2308983f4a2453
danascheider <dana.scheider@gmail.com>
2014-05-26 15:26:27 -0700

    Validate presence of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :todo_items, dependent: :destroy, foreign_key: :user_id
   validates_associated :todo_items
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 end
