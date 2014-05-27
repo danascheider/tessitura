71fdfa32b89782a79fa420e3d4994586fd63fce3
danascheider <dana.scheider@gmail.com>
2014-05-27 14:19:31 -0700

    Define step 'login link should take me to the login page'

--- features/step_definitions/user_login_steps.rb
+++ features/step_definitions/user_login_steps.rb
@@ -1,3 +1,8 @@
 Then /^there should be a link to log in$/ do 
   page.should have_css("a#login")
 end
+
+Then /^the '(.*)' link should take me to the (.*) page$/ do |link_text, page|
+  click_link(link_text)
+  current_path.should == welcome_login_path
+end
