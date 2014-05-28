000df78ddaec0bf0c420fa9c9068bfa17280ced4
danascheider <dana.scheider@gmail.com>
2014-05-28 11:30:52 -0700

    Eliminate most user examples to start over

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,34 +1,4 @@
 require "spec_helper"
 
 describe User do 
-  describe 'validating a user' do 
-    before :each do 
-      @user = User.new(username: 'frank', password_digest: 'mypasswd1', email: 'frank@example.com')
-    end
-
-    it "is invalid without a username" do 
-      @user.username = nil
-      expect(@user).not_to be_valid
-    end
-
-    it "is invalid without a password" do 
-      @user.password_digest = nil
-      expect(@user).not_to be_valid
-    end
-
-    it "is invalid without an e-mail" do 
-      @user.email = nil
-      expect(@user).not_to be_valid
-    end
-
-    it "has a unique username" do 
-      @user.save
-      new_user = User.new(username: 'frank', password_digest: 'mypasswd2', email: 'felix@example.com')
-      expect(new_user).not_to be_valid
-    end
-
-    it "is not an admin by default" do 
-      expect(@user).not_to be_admin
-    end
-  end
 end
