703c080c22c261739533b187468013185f7a1cfb
danascheider <dana.scheider@gmail.com>
2014-05-29 13:32:43 -0700

    Total do-over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Canto::Application.config.secret_key_base = 'b321859ad13b9e7cc5790f1affbea4453d3812d67d77e37ac16d875df7aa91fe644edef5ce06934fd72861db4234d0c330e5dbafa2a057c1e0607664c8998b49'
