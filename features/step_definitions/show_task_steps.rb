b8caa0e7611b731860060749b0d4132a58ac0198
danascheider <dana.scheider@gmail.com>
2014-05-30 16:30:52 -0700

    Define 'see the tasks title and status'

--- features/step_definitions/show_task_steps.rb
+++ features/step_definitions/show_task_steps.rb
@@ -12,3 +12,8 @@ end
 Then /^I should go to that item's show page$/ do 
   current_path.should eql task_path(@task)
 end
+
+Then /^I should see the task's title and status$/ do 
+  find('table').should have_content(@task.title)
+  find('table').should have_content(@task.complete)
+end
