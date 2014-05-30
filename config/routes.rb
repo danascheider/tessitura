0ae48854fcdf927bb9cdb86b39f88c11fceb7f02
danascheider <dana.scheider@gmail.com>
2014-05-29 23:12:17 -0700

    Make task list root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Canto::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'tasks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
