0fb2adee436a70152f1a220d077e801707df9412
danascheider <dana.scheider@gmail.com>
2014-05-30 14:39:14 -0700

    Add 'incomplete' scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   validates :title, presence: true
   scope :complete, -> { where(complete: true) }
+  scope :incomplete, -> { where(complete: false) }
 
   def complete?
     self.complete
