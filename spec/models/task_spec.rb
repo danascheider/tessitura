393df34e7707f091d8eff67ea2103f5bbfb81b43
danascheider <dana.scheider@gmail.com>
2014-05-30 12:14:15 -0700

    Test validation of title (presence)

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,14 @@ require 'spec_helper'
 describe Task do
   it { should respond_to(:title) }
   it { should respond_to(:complete? ) }
+
+  describe 'validations' do 
+    before :each do
+      @task = Task.new
+    end
+    
+    it 'is invalid without a title' do 
+      expect(@task).not_to be_valid
+    end
+  end
 end
