419ab33a7f6c894084e94c3de027139c6877b9f4
danascheider <dana.scheider@gmail.com>
2014-05-30 16:21:01 -0700

    Define step 'click show link'

--- features/step_definitions/show_task_steps.rb
+++ features/step_definitions/show_task_steps.rb
@@ -1,3 +1,10 @@
 Given /^I'm viewing my to\-do list$/ do 
   visit tasks_path
 end
+
+When /^I click a task's '(.*)' link$/ do |link_text|
+  @task = Task.find_by_id(2)
+  within('table') do 
+    click_link(link_text, href: task_path(@task))
+  end
+end
