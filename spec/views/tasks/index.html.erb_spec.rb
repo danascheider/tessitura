e491194eeb6816ad2c5e6a119727a6456bc0ee42
danascheider <dana.scheider@gmail.com>
2014-05-30 12:28:08 -0700

    Check for no-tasks message

--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -17,4 +17,16 @@ describe "tasks/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Title".to_s, :count => 2
   end
+
+  describe "when there are no tasks" do 
+
+    # Make sure task list is empty
+    before(:each) do 
+      Task.all == nil
+    end
+
+    it "displays a message that there are no tasks" do 
+      assert_select "p", text: 'No tasks!'
+    end
+  end
 end
