688aa2410f3ea2faf00eea250c1a5ff6459fcc34
danascheider <dana.scheider@gmail.com>
2014-05-31 13:44:33 -0700

    Define step 'click the checkbox'

--- features/step_definitions/edit_task_steps.rb
+++ features/step_definitions/edit_task_steps.rb
@@ -2,6 +2,8 @@ Given /^there is a task called "(.*)"$/ do |title|
   @task = FactoryGirl.create(:task, title: title)
 end
 
+### Scenario 1
+
 Given /^I am on its edit page$/ do
   visit edit_task_path(@task)
 end
@@ -23,4 +25,11 @@ end
 
 Then /^I should see a message that the (.*) was changed$/ do |attribute|
   find('#notice').should have_content("Task was successfully updated")
+end
+
+### Scenario 2
+
+When /^I click the checkbox next to the "(.*)" task$/ do |title|
+  @task = Task.find_by(title: title)
+  check("complete-task#{@task.id}")
 end
