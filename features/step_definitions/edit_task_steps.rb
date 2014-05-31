b7559a1641af8f1d40984829de14e2c97d27eb85
danascheider <dana.scheider@gmail.com>
2014-05-30 17:35:38 -0700

    Define step 'change its title'

--- features/step_definitions/edit_task_steps.rb
+++ features/step_definitions/edit_task_steps.rb
@@ -5,3 +5,8 @@ end
 Given /^I am on its edit page$/ do
   visit edit_task_path(@task)
 end
+
+When /^I change its title to "(.*)"$/ do |title|
+  fill_in 'Title', with: title
+  click_button 'Update Task'
+end
