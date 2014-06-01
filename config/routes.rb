f4849b381d4e355068ca783897efaeb1c4ef6334
danascheider <dana.scheider@gmail.com>
2014-05-31 18:38:57 -0700

    Move root route to the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Canto::Application.routes.draw do
+
+  root 'tasks#index'
+
   resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'tasks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
