b84eb7bba4fa016757777b5ec90ae0038227a2ba
danascheider <dana.scheider@gmail.com>
2014-06-05 10:57:48 -0700

    Remove 'debugger' statement in mark_complete, correct typo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @tasks_url, notice: 'Task was successfully created.' }
+        format.html { redirect_to tasks_url, notice: 'Task was successfully created.' }
         format.json { render action: 'show', status: :created, location: @task }
       else
         format.html { render action: 'new' }
@@ -62,7 +62,6 @@ class TasksController < ApplicationController
   end
 
   def mark_complete
-    debugger
     @task.update(complete: true)
     respond_to do |format|
       format.html { render action: 'index' }
