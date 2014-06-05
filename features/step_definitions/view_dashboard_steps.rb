c7a70294233c6b19fa57e6b9263a87a10467a75b
danascheider <dana.scheider@gmail.com>
2014-06-05 14:15:21 -0700

    Define step 'I should not be redirected'

--- features/step_definitions/view_dashboard_steps.rb
+++ features/step_definitions/view_dashboard_steps.rb
@@ -4,8 +4,11 @@ end
 
 When /^I mark the "(.*)" task complete$/ do |title|
   @task = Task.find_by(title: title)
-  form_id = "#task-#{@task.id}"
-  within(form_id) do 
+  within("#task-#{@task.id}") do 
     click_on 'Mark Complete'
   end
+end
+
+Then /^I should not be redirected$/ do 
+  expect(current_path).to eql dashboard_path
 end
