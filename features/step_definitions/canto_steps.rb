587e464dd28125124d03713bc4ebec394aeaec75
danascheider <dana.scheider@gmail.com>
2014-06-05 22:21:03 -0700

    Move step 'there are (\d) tasks' to canto_steps

--- features/step_definitions/canto_steps.rb
+++ features/step_definitions/canto_steps.rb
@@ -2,6 +2,15 @@ Given /^I'm viewing my to\-do list$/ do
   visit tasks_path
 end
 
+Given /^there are (no||\d+) tasks$/ do |number|
+  if number == 'no' || number == 0
+    Task.count == 0
+  else
+    @task_list = [] 
+    number.to_i.times {|i| @task_list << FactoryGirl.create(:task, title: "Task #{i}")}
+  end
+end
+
 When /^I click a task's '(.*)' link$/ do |link_text|
   @task = Task.find_by_id(2)
   within('table') do 
