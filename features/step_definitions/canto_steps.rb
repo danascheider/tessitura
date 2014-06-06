1665a83d01875a506b7683039658169956b49990
danascheider <dana.scheider@gmail.com>
2014-06-05 22:18:59 -0700

    Move 'submit the form with the title (.*)' to canto_steps

--- features/step_definitions/canto_steps.rb
+++ features/step_definitions/canto_steps.rb
@@ -8,3 +8,8 @@ When /^I click a task's '(.*)' link$/ do |link_text|
     click_link(link_text, href: task_path(@task))
   end
 end
+
+When /^I submit the form with the title (.*)$/ do |title|
+  fill_in 'Title', with: title 
+  click_button 'Create Task'
+end
--- features/step_definitions/create_task_steps.rb
+++ features/step_definitions/create_task_steps.rb
@@ -2,11 +2,6 @@ Given /^I have navigated to the 'New Task' page$/ do
   visit new_task_path
 end
 
-When /^I submit the form with the title (.*)$/ do |title|
-  fill_in 'Title', with: title 
-  click_button 'Create Task'
-end
-
 Then /^a new task should be created with the title (.*)$/ do |title|
   Task.count.should == 5 
   Task.last.title.should eql title
