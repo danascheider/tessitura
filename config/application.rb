79eae09eb55c5dd0507a1d87a4ac6daee8001143
danascheider <dana.scheider@gmail.com>
2014-06-07 14:29:52 -0700

    Make Bootstrap work

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,16 @@ Bundler.require(:default, Rails.env)
 
 module Canto
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Resolve some issues with 
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
+      "<div class=\"has-error form-group\">#{html_tag}</div>".html_safe
+    end
+
+    # Default time zone is Pacific (west-coast US)
+    # Settings in config/environments/* take precedence over those
+    # specified here.
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
