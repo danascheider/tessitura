a7fd04f09d482caf7245652ebb6493ccca5c746d
danascheider <dana.scheider@gmail.com>
2014-06-08 16:41:17 -0700

    Define create_widget() and include_icon() methods

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,13 @@
 module DashboardHelper
+  def create_widget(opts = {})
+    @title = opts[:title]
+    @icon_class = opts[:icon_class]
+    @partial = opts[:partial]
+    @widget_id = opts[:id]
+    render 'widget'
+  end
+
+  def include_icon(icon_class)
+    tag("i", class: icon_class) + tag("/i")
+  end
 end
