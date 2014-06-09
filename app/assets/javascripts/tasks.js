582f804a5c1705ead32cccd36d4a0c699d92faef
danascheider <dana.scheider@gmail.com>
2014-06-08 21:51:37 -0700

    Remove unused JavaScripts from project (still in sb-admin folder)

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,9 +3,3 @@
 
 // This doesn't affect the behavior of the app right now. This is in 
 // the issue tracking system.
-
-$(function() {
-  $(".todo-list-item li").click(function(){
-    $(this).children('.task-details').css('display', 'inherit');
-  });
-});
