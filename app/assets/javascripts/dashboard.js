650381fddbfedbfddaf0286fee7cf477f7db9fb2
danascheider <dana.scheider@gmail.com>
2014-06-08 22:29:34 -0700

    Delete non-working function

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,8 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(function() {
-  $("li.todo-list-item").click(function() {
-    $(this).children(".task-details").css("display", "inherit");
-  });
-});
