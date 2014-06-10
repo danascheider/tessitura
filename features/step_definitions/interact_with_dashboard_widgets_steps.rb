34db5fa48c30e71f0763cc69d2afd47416dbbf62
danascheider <dana.scheider@gmail.com>
2014-06-10 16:20:59 -0700

    Add selector to 'within' statement

--- features/step_definitions/interact_with_dashboard_widgets_steps.rb
+++ features/step_definitions/interact_with_dashboard_widgets_steps.rb
@@ -1,6 +1,6 @@
 When /^I mark the "(.*)" task complete$/ do |title|
   @task = Task.find_by(title: title)
-  within("#task-#{@task.id}") do 
+  within("li#task-#{@task.id}") do 
     click_button ''
   end
 end
