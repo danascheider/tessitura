942bc8bb4869228fd6c6622f79c1fb96e5d73c83
danascheider <dana.scheider@gmail.com>
2014-06-16 15:21:15 -0700

    Redirect update action to root url - temporary solution to Ajax problem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: 'Task was successfully updated.' }
+        format.html { redirect_to root_url notice: 'Task was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
