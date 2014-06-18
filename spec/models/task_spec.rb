e024b30675ae01170b575296c4e4140d04df7334
danascheider <dana.scheider@gmail.com>
2014-06-18 11:53:04 -0700

    Add back model specs

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe Task do
+  it { should respond_to(:title) }
+  it { should respond_to(:complete? ) }
+  it { should respond_to(:incomplete? ) }
+
+  describe 'validations' do 
+    before :each do
+      @task = Task.new
+    end
+    
+    it 'is invalid without a title' do 
+      expect(@task).not_to be_valid
+    end
+  end
+
+  describe 'scope "complete"' do 
+    it 'returns all completed tasks' do 
+      Task.complete.should == Task.where(complete: true)
+    end
+  end
+
+  describe 'scope "incomplete"' do 
+    it 'returns all incomplete tasks' do 
+      Task.incomplete.should == Task.where(complete: false)
+    end
+  end
+
+  describe 'default value of :complete' do 
+    it 'is created with complete: false' do 
+      t = Task.create(title: 'Test Task')
+      t.complete.should be false
+    end
+  end
+end
