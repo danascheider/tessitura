d138328dc17ec86e12b344c529e64b0e4c2186c1
danascheider <dana.scheider@gmail.com>
2014-06-21 18:31:58 -0700

    Create task form helper and incorporate shared task form into views

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,21 @@
 module TasksHelper
+  def task_form(task, opts = {})
+    @task   = task
+    case opts[:action]
+      when :create
+        @method       = 'post'
+        @destination  = tasks_path
+        @button_value = 'Create Task'
+
+      when :update
+        @method       = 'patch'
+        @destination  = task_path(@task)
+        @button_value = 'Update Task'
+
+      else
+        raise ArgumentError, 'task_form() can only handle :create and :update actions'
+    end
+
+    render 'shared/task_form'
+  end
 end
