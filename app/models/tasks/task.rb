b124d71711ae8b0a5928cdc31ffd08790dfc45dc
danascheider <dana.scheider@gmail.com>
2014-06-26 18:55:32 -0700

    Add :complete? method

--- app/models/tasks/task.rb
+++ app/models/tasks/task.rb
@@ -1,7 +1,11 @@
 class Task < ActiveRecord::Base
   scope :complete, -> { where(complete: true)}
   scope :incomplete, -> {where(complete: false)}
-  
+
+  def complete?
+    self.complete
+  end
+
   def incomplete?
     !self.complete
   end
