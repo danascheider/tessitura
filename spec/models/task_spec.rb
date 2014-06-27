1c1beffbbb9b17de85c02c6620ee2215ae46f65b
danascheider <dana.scheider@gmail.com>
2014-06-26 20:18:37 -0700

    Add example 'respond_to :to_hash'

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Task do 
   it { should respond_to(:title) }
   it { should respond_to(:incomplete?) }
+  it { should respond_to(:to_hash) } # to integrate with Sinatra-Backbone
 end
