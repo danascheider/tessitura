526f807bb45a2ce5ec5d6ee01916c326faa4d439
danascheider <dana.scheider@gmail.com>
2014-06-26 18:43:13 -0700

    Require factory girl and factories

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,11 @@ require File.join(File.dirname(__FILE__), '..', '..', './canto.rb')
 
 require 'capybara'
 require 'capybara/cucumber'
+require 'factory_girl'
 require 'rspec'
+
+# Require all factories from RSpec directory
+require_all File.expand_path('../../../spec/factories', __FILE__)
 
 Capybara.app = Canto
 
