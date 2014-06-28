1dea63af7970e35dc555d94c483aab5930931712
danascheider <dana.scheider@gmail.com>
2014-06-27 17:23:14 -0700

    Add example for validations - invalid w/o title

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,4 +5,11 @@ describe Task do
   it { should respond_to(:complete) }
   it { should respond_to(:incomplete?) }
   it { should respond_to(:to_hash) } # to integrate with Sinatra-Backbone
+
+  describe 'validations' do 
+    it 'is invalid without a title' do 
+      @task = Task.new
+      expect(@task).not_to be_valid
+    end
+  end
 end
