ce53dd460a0a3187a8b19221ef8dc9b3dbae0abe
danascheider <dana.scheider@gmail.com>
2014-06-27 17:17:51 -0700

    Add to_hash method

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Task do 
   it { should respond_to(:title) }
+  it { should respond_to(:complete) }
   it { should respond_to(:incomplete?) }
   it { should respond_to(:to_hash) } # to integrate with Sinatra-Backbone
 end
