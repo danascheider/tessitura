1c633a3433d5acaeb3d13f20f71d568cc51799d6
danascheider <dana.scheider@gmail.com>
2014-06-27 17:17:31 -0700

    Add to_hash method

--- models/task.rb
+++ models/task.rb
@@ -3,4 +3,13 @@ class Task < ActiveRecord::Base
   def incomplete?
     !self.complete
   end
+
+  def to_hash
+    { id: self.id,
+      title: self.title, 
+      complete: self.complete, 
+      created_at: self.created_at,
+      updated_at: self.updated_at
+    }
+  end
 end
