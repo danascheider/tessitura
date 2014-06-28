e5a0148b65adc9ce9773122479110c06e4d5e65b
danascheider <dana.scheider@gmail.com>
2014-06-27 17:04:37 -0700

    Add tasks table to schema via migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140628000210) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.boolean  "complete"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
