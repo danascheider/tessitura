9c64f51e12428f1387e2ef389cc88e11a5220694
danascheider <dana.scheider@gmail.com>
2014-06-29 13:52:19 -0700

    Remove step definition handled by json_spec gem

--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -1,7 +1,3 @@
 When(/^the client requests GET \/(.*)$/) do |path|
   get(path)
 end
-
-Then(/^the response should be JSON:$/) do |string|
-  pending # express the regexp above with the code you wish you had
-end
