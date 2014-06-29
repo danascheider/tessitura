11c24c570ef23d584f11f01e85e3206a89d49bd2
danascheider <dana.scheider@gmail.com>
2014-06-29 13:09:25 -0700

    Define get '/tasks'; set :data

--- canto.rb
+++ canto.rb
@@ -6,4 +6,11 @@ require_all 'models'
 
 class Canto < Sinatra::Application
   set :database_file, 'config/database.yml'
+
+  set :data, ''
+
+  get '/tasks' do 
+    content_type :json
+    Task.all.to_json
+  end
 end
