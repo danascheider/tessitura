fd43436e94602a328eb1c66edac8899b3995ab86
danascheider <dana.scheider@gmail.com>
2014-06-29 15:26:56 -0700

    Write example dictating that complete should be false not nil

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,10 +6,21 @@ describe Task do
   it { should respond_to(:incomplete?) }
   it { should respond_to(:to_hash) } # to integrate with Sinatra-Backbone
 
+  before(:each) do 
+    @task = Task.new 
+  end
+
   describe 'validations' do 
     it 'is invalid without a title' do 
-      @task = Task.new
       expect(@task).not_to be_valid
+    end
+  end
+
+  describe 'default behavior' do 
+    it 'sets :complete to false, not nil' do 
+      @task.title = 'Walk the dog'
+      @task.save!
+      expect(@task.complete).to eql false
     end
   end
 end
