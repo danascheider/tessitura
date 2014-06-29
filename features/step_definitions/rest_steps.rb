b95e2dc855281e09bbd76a93dcb8a9e6338e6eb3
danascheider <dana.scheider@gmail.com>
2014-06-29 14:46:54 -0700

    Change @tasks.to_json to Task.all.to_json to match actual output (which I preferred over the output of @tasks.to_json)

--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -3,5 +3,5 @@ When(/^the client requests GET \/(.*)$/) do |path|
 end
 
 Then(/^the JSON response should include all the tasks$/) do 
-  last_response.body.should === @tasks.to_json
+  last_response.body.should === Task.all.to_json
 end
