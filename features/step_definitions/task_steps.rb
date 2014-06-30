d47666962681804c723fa453457441a168b80696
danascheider <dana.scheider@gmail.com>
2014-06-29 18:19:22 -0700

    Define step where task should be created

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -4,3 +4,7 @@ Given(/^there are the following tasks:$/) do |tasks|
     task = Task.create(title: task['title'], complete: task['complete'])
   end
 end
+
+Then(/^a new task should be created with the title '(.*)'$/) do |title|
+  expect(Task.find_by(title: title)).not_to be_nil
+end
