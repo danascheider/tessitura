4a2cb9fae119894feab26d81bbacbcef6896264c
danascheider <dana.scheider@gmail.com>
2014-06-29 17:28:25 -0700

    Configure json_spec gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require          'factory_girl'
+require          'json_spec/helpers'
 require_relative '../canto'
 require_all      File.dirname(__FILE__) + '/factories'
+
+RSpec.configure do |config|
+  config.include JsonSpec::Helpers
+end
