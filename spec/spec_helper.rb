dd630b6c2d9da19d2d10c87a3fea8f8394692ebc
danascheider <dana.scheider@gmail.com>
2014-06-29 17:29:07 -0700

    Add rack-test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require          'factory_girl'
 require          'json_spec/helpers'
+require          'rack/test'
 require_relative '../canto'
 require_all      File.dirname(__FILE__) + '/factories'
 
