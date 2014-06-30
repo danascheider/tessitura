6a46e86170c4ce1cccce3fddf568f23446607773
danascheider <dana.scheider@gmail.com>
2014-06-29 17:41:12 -0700

    Configure Database Cleaner and set environment to Test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,29 @@
+ENV['RACK_ENV'] = 'test'
+
 require          'factory_girl'
 require          'json_spec/helpers'
 require          'rack/test'
+require          'database_cleaner'
 require_relative '../canto'
 require_all      File.dirname(__FILE__) + '/factories'
 
 RSpec.configure do |config|
   config.include JsonSpec::Helpers
+  config.include Rack::Test::Methods
+
+  config.before(:suite) do 
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do 
+      example.run
+    end
+  end
+
+end
+
+def app
+  Sinatra::Application
 end
