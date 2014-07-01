10caee9d1da12444fb09b4511c37b589810432ff
danascheider <dana.scheider@gmail.com>
2014-07-01 16:47:09 -0700

    Change ordinal endings to non-capturing patterns

--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -40,11 +40,11 @@ Then(/^the JSON response should include all the tasks$/) do
   last_response.body.should === Task.all.to_json
 end
 
-Then(/^the JSON response should include only the (\d+)(.{2}) task$/) do |id, ordinal|
+Then(/^the JSON response should include only the (\d+)(?:.{2}) task$/) do |id|
   last_response.body.should === Task.find(id).to_json
 end
 
-Then(/^the JSON response should not include the (\d+)(.{2}) task$/) do |id, ordinal|
+Then(/^the JSON response should not include the (\d+)(?:.{2}) task$/) do |id|
   last_response.body.should_not include(Task.find(id).to_json)
 end
 
