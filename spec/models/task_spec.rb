21e496434abe0bcc7f57f53d10c70fba72688056
danascheider <dana.scheider@gmail.com>
2014-07-02 13:24:00 -0700

    Reorganize hooks so as not to pollute the test environment

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,67 +7,72 @@ describe Task do
   it { should respond_to(:incomplete?) }
   it { should respond_to(:to_hash) } # to integrate with Sinatra-Backbone
 
-  before(:each) do 
-    @task = Task.new 
-  end
-
   describe 'validations' do 
     context ' pertaining to the index' do
       before(:all) do 
         Task.create!(title: 'Filo mio')
-        Task.create!(title: 'My string')
+        @task = Task.create!(title: 'My string')
+        puts "This is in the before(:all) block"
+        puts "@task = #{@task.to_hash}"
       end
 
       after(:all) do 
-        Task.where.not(id: 1).destroy
+        Task.all.destroy
       end
 
       it 'prioritizes the new task\'s index' do 
-        Task.create!(title: 'Feed the fish', index: 2)
-        expect(Task.last.index).to eql 2
+        puts "@task = #{@task.to_hash}"
+        new_task = Task.create!(title: 'Feed the fish', index: 2)
+        expect(new_task.index).to eql 2
       end
 
       it 'changes the index of the other task' do 
         expect(get_changed.index).to eql 3
       end
-    end
+    end 
 
-    it 'is invalid without a title' do 
-      expect(@task).not_to be_valid
-    end
+    context 'pertaining to title' do 
+      before(:each) do 
+        puts "This is the before(:each) hook"
+        @task = Task.new
+      end
 
-    it 'instantiates with valid attributes' do 
-      @task.title = 'Call mom'
-      expect(@task.save!) 
-    end
+      it 'is invalid without a title' do 
+        expect(@task).not_to be_valid
+      end
 
-    it 'doesn\'t update without a title' do 
-      task = Task.create(title: 'Go to Fred Meyer')
-      expect(!task.update(title: nil))
-    end
+      it 'instantiates with valid attributes' do 
+        @task.title = 'Call mom'
+        expect(@task.save!) 
+      end
 
-    it 'updates with valid attributes' do 
-      task = Task.create(title: 'Go to Fred Meyer')
-      expect(task.update(title: 'Go to Home Depot'))
+      it 'doesn\'t update without a title' do 
+        task = Task.create(title: 'Go to Fred Meyer')
+        expect(!task.update(title: nil))
+      end
+
+      it 'updates with valid attributes' do 
+        task = Task.create(title: 'Go to Fred Meyer')
+        expect(task.update(title: 'Go to Home Depot'))
+      end
     end
   end
 
   describe 'default behavior' do 
+    before(:each) do 
+      @task = Task.create!(title: 'Walk the dog')
+    end
+
     it 'instantiates at index 1' do 
-      @task.title = 'Walk the dog'
-      @task.save!
       expect(@task.index).to eql 1
     end
 
-    it 'sets :complete to false, not nil' do 
-      @task.title = 'Walk the dog'
-      @task.save!
+    it 'sets :complete to false, not nil' do !
       expect(@task.complete).to eql false
     end
 
     it 'sets :complete to false only if actually false' do 
-      @task.complete, @task.title = true, 'Walk the dog'
-      @task.save!
+      @task.update(complete: true, title: 'Go to the store')
       expect(@task.complete).to eql true
     end
   end
