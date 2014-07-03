2aa63b56147c418430217f1f16db5efd348a6234
danascheider <dana.scheider@gmail.com>
2014-07-02 17:17:03 -0700

    Set index when creating tasks

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -1,7 +1,8 @@
 Given(/^there are the following tasks:$/) do |tasks|
-  @tasks = tasks.hashes
-  @tasks.each do |task|
-    task = Task.create(title: task['title'], complete: task['complete'])
+  tasks.hashes.each do |task|
+    task = Task.create(title: task['title'], 
+                       complete: task['complete'], 
+                       index: task['index'])
   end
 end
 
