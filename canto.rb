6fe8d5bf8c1718b131aeac6181709d833fbe72ba
danascheider <dana.scheider@gmail.com>
2014-07-02 20:52:37 -0700

    Remove no-longer-relevant comment

--- canto.rb
+++ canto.rb
@@ -9,8 +9,6 @@ class Canto < Sinatra::Application
   set :database_file, 'config/database.yml'
   set :data, ''
 
-  # begin_and_rescue method is defined in the ErrorHandling helper module
-
   get '/tasks' do 
     content_type :json
     params[:complete] ? find_by(:complete, params[:complete]).to_json : Task.all.to_json
