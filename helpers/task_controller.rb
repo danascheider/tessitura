8c8126af584ad041026dd654caf68273cd59ba05
danascheider <dana.scheider@gmail.com>
2014-07-02 21:00:48 -0700

    Modify update method to handle changing indices

--- helpers/task_controller.rb
+++ helpers/task_controller.rb
@@ -2,6 +2,11 @@ class Sinatra::Application
 
   module TaskController
 
+    # The begin_and_rescue method is defined in the ErrorHandling module.
+
+    # BASIC CONTROLLER METHODS
+    # ========================
+
     def create_task(body)
       begin_and_rescue(ActiveRecord::RecordInvalid, 422) { Task.create!(body) && 201 }
     end
@@ -11,18 +16,30 @@ class Sinatra::Application
     end
 
     def update_task(id, body)
-      begin_and_rescue(ActiveRecord::RecordInvalid, 422) { find_task(id).update!(body) }
+      begin_and_rescue(ActiveRecord::RecordInvalid, 422) do 
+        @task = find_task(id)
+        update_indices(body[:index]) if change_index? body
+        @task.update!(body)
+      end
     end
 
     def delete_task(id)
       begin_and_rescue(ActiveRecord::RecordNotFound, 404) { find_task(id).destroy && 204 }
     end
 
+    # HELPER METHODS
+    # ==============
+
     def update_indices(index)
       Task.all.each do |task|
         if task.index <= index then task.index -= 1 && task.save!; end
       end
     end
+
+    private 
+      def change_index?(object)
+        object.has_key? :index && object[:index] != @task.index
+      end
 
   end
 
