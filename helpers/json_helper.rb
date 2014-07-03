7543d0eb5725f86de4b7cab6f0ddba65b7b3b166
danascheider <dana.scheider@gmail.com>
2014-07-02 20:52:03 -0700

    Symbolize keys when parsing JSON

--- helpers/json_helper.rb
+++ helpers/json_helper.rb
@@ -1,7 +1,7 @@
 class Sinatra::Application
   module JSONHelper
     def request_body
-      JSON.parse request.body.read
+      JSON.parse request.body.read, symbolize_names: true
     end
   end
 
