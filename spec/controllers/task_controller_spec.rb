58531e9232d87f68c3b808fc662cbba2f5bc2e61
danascheider <dana.scheider@gmail.com>
2014-07-03 21:20:29 -0700

    Create spec - still buggy

--- spec/controllers/task_controller_spec.rb
+++ spec/controllers/task_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Canto do 
   context 'TaskController' do 
-    #
-  end
-end
+    describe 'task indexing functions' do 
+      describe 'CREATE method' do 
+        context 'index not explicitly set' do 
+        end # 'index not explicitly set'
+      end # CREATE method
+    end # task indexing functions
+  end # context 'TaskController'
+end # Canto
