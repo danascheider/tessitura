8f7c3b2f757f943e5f26b201bd89bb3ccf98f5d0
danascheider <dana.scheider@gmail.com>
2014-07-03 23:02:06 -0700

    Fix some typos and reword examples to make bugs go away

--- spec/controllers/task_controller_spec.rb
+++ spec/controllers/task_controller_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Canto do 
   context 'TaskController' do 
     describe 'task indexing functions' do 
+
       describe 'CREATE method' do 
         context 'index not explicitly set' do           
           before(:all) do 
             Task.create!(title: 'My task 1', index: 1)
-            Task.create!(title: 'My task 2', index: 2)
           end
 
           after(:all) do 
-            Task.all.destroy
+            Task.all.each {|task| task.destroy }
           end
 
           it 'sets new task\'s index to 1 by default' do 
@@ -19,11 +19,32 @@ describe Canto do
             expect(task.index).to eql 1
           end
 
-          it 'increases index of all the other tasks by 1' do
+          it 'increases index of other tasks by 1' do
             expect(Task.find(1).index).to eql 2
-            expect(Task.find(2).index).to eql 3
           end
         end # 'index not explicitly set'
+
+        context 'index explicitly set' do 
+          before(:all) do 
+            for i in 1..4
+              Task.create!(title: "My task #{i}", index: i)
+            end
+          end
+
+          after(:all) do 
+            Task.all.each {|task| task.destroy }
+          end
+
+          it 'sets the task\'s index to the one specified' do 
+            @task = Task.create!(title: "My new task", index: 3)
+            expect(@task.index).to eql 3
+            Task.all.to_a.each {|task| puts "#{task.to_hash}\n"}
+          end
+
+          it 'increases index of 3rd and 4th tasks by 1' do 
+            expect(neg_task_scope(id: @task.id)).to eql [1, 2, 4, 5]
+          end
+        end # index explicitly set
       end # CREATE method
     end # task indexing functions
   end # context 'TaskController'
