474acc2884f8a947e8f0f8bc439dfbbd6e774d05
danascheider <dana.scheider@gmail.com>
2014-07-10 12:31:14 -0700

    Create task list table to use with acts_as_list

---
+++ db/migrate/20140710192647_create_task_list.rb
@@ -0,0 +1,6 @@
+class CreateTaskList < ActiveRecord::Migration
+  def change
+    create_table :task_list do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710190414) do
+ActiveRecord::Schema.define(version: 20140710192647) do
+
+  create_table "task_list", force: true do |t|
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title"
