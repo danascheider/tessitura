36955b4ba144d44bd4791c865660bb2c26117236
danascheider <dana.scheider@gmail.com>
2014-07-11 14:59:20 -0700

    Remove unneded interpret_complete_param method

--- helpers/general_helper_methods.rb
+++ helpers/general_helper_methods.rb
@@ -3,10 +3,6 @@ class Sinatra::Application
     def to_bool(string)
       string == 'false' || string == 'nil' ? false : true
     end
-
-    def interpret_complete_param(string)
-      to_bool(string) == true ? Task.complete.to_json : Task.incomplete.to_json 
-    end
   end
 
   helpers GeneralHelperMethods
