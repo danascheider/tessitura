ec86ab32da08167d048666720c864ef27b6f8f8b
danascheider <dana.scheider@gmail.com>
2014-07-11 11:03:59 -0700

    Add param helper with validate_params method, tests for same

---
+++ helpers/params_helper.rb
@@ -0,0 +1,12 @@
+class Sinatra::Application
+  module ParamsHelper
+    ALLOWED_PARAMS = [ 'complete', 'status', 'priority', 'deadline', 'title', 'created', 'updated']
+
+    def validate_params(params)
+      params.delete_if {|key, value| !ALLOWED_PARAMS.include? key }
+      params
+    end
+  end
+
+  helpers ParamsHelper
+end
