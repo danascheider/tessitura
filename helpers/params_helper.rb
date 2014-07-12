bf161ab7ef7ecf58549825302c387be28354911d
danascheider <dana.scheider@gmail.com>
2014-07-11 17:53:02 -0700

    validate params before responding to requests

--- helpers/params_helper.rb
+++ helpers/params_helper.rb
@@ -2,7 +2,7 @@ class Sinatra::Application
   module ParamsHelper
     ALLOWED_PARAMS = [ 'complete', 'status', 'priority', 'deadline', 'title', 'created', 'updated' ]
 
-    def validate_params(params)
+    def validate_params(params={})
       params.delete_if {|key, value| !ALLOWED_PARAMS.include? key }
     end
 
