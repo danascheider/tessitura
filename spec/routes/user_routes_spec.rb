0e94cb51f267cc56918013cfa2e630ecf2981bf2
danascheider <dana.scheider@gmail.com>
2014-07-24 16:35:34 -0700

    Add examples for POST method to /users

--- spec/routes/user_routes_spec.rb
+++ spec/routes/user_routes_spec.rb
@@ -4,5 +4,14 @@ describe Canto do
   include Rack::Test::Methods
 
   describe 'POST' do 
+    context 'with valid attributes' do 
+      it 'returns an API code' do 
+        expect(response_body).to have_key("secret_key")
+      end
+
+      it 'returns status code 201' do 
+        expect(response_status).to eql 201
+      end
+    end
   end
 end
