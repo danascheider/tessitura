a958d7461b750e7c13a343ad62a7b4d6dcec4155
danascheider <dana.scheider@gmail.com>
2014-07-24 15:47:47 -0700

    Define step checking for admin status

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,11 @@
 Given(/^there are (\d+|no) users$/) do |number|
   number == 'no' ? User.count == 0 : number.times { FactoryGirl.create(:user) }
 end
+
+# ADMIN STEPS
+# ===========
+
+Then(/^the user named '(\w+) (\w+)' should be an admin$/) do |first, last|
+  @user = User.find_by(first_name: first, last_name: last)
+  @user.should be_admin
+end
