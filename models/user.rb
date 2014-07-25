fddbd38c07f8503ef68cbda785cbcf10a58e2cc8
danascheider <dana.scheider@gmail.com>
2014-07-24 17:01:13 -0700

    Issue secret key on user creation

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,9 @@
+require 'securerandom'
+
 class User < ActiveRecord::Base
   before_create :set_admin_status
+  before_create :issue_api_key
+
   def admin?
     self.admin 
   end
@@ -9,6 +13,10 @@ class User < ActiveRecord::Base
   end
 
   private
+    def issue_api_key
+      self.secret_key = SecureRandom.urlsafe_base64(30)
+    end
+
     def set_admin_status
       self.admin = true if User.count == 0
     end
