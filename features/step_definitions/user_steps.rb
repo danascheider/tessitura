d84625b1c0157d0342a8d451ff98575b131a67b9
danascheider <dana.scheider@gmail.com>
2014-07-24 17:26:26 -0700

    Define another step

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -9,6 +9,10 @@ Then(/^the user named '(\w+) (\w+)' should be an admin$/) do |first, last|
   User.find_by(first_name: first, last_name: last).should be_admin
 end
 
+Then(/^the (\d+)(?:[a-z]{2}) user should (not )?be an admin$/) do |id, neg|
+  User.find(id).should be_admin unless neg
+end
+
 # USER CREATION
 # =============
 
