e130f4c3f1f9e220e31645c93d54c8d80bbe9fa1
danascheider <dana.scheider@gmail.com>
2014-07-25 11:57:05 -0700

    Minor rewording and debugging

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,14 +1,15 @@
 Given(/^there are (\d+|no) users$/) do |number|
   # @user_count is used in 'no user should be created', below
   number == 'no' ? User.count == 0 : number.times { FactoryGirl.create(:user) }
+  User.first.update(secret_key: '12345abcde1') unless number == 'no'
   @user_count = User.count
 end
 
 # ADMIN STEPS
 # ===========
 
-Then(/^the user named '(\w+) (\w+)' should be an admin$/) do |first, last|
-  User.find_by(first_name: first, last_name: last).should be_admin
+Then(/^the user should be an admin$/) do
+  @user.should be_admin
 end
 
 Then(/^the (\d+)(?:[a-z]{2}) user should (not )?be an admin$/) do |id, neg|
@@ -30,7 +31,8 @@ Then(/^a new user should be created with the following attributes:$/) do |attrib
 end
 
 Then(/^a user named '(\w+) (\w+)' should be created$/) do |first, last|
-  User.find_by(first_name: first, last_name: last).should_not eql nil
+  # @user is called in 'the user should be an admin'
+  (@user = User.find_by(first_name: first, last_name: last)).should_not eql nil
 end
 
 Then(/^no user should be created$/) do 
