c17b7497bf009c94115b7183871769d252c7071b
danascheider <dana.scheider@gmail.com>
2014-07-25 22:08:56 -0700

    Extract methods, clean up

--- helpers/authorization_helper.rb
+++ helpers/authorization_helper.rb
@@ -1,15 +1,25 @@
 class Sinatra::Application
   module AuthorizationHelper
+    def authorized_user?(id, body)
+      key = body[:secret_key]
+      return nil unless valid_key?(key)
+      User.is_admin_key?(key) || (User.find_by(secret_key: key).id == id && !body[:admin])
+    end
+
     def validate_and_create_user(body)
       (valid_admin_creation?(body) || !body.has_key?(:admin)) ? User.create!(body) : nil
     end
 
     def validate_and_update_user(id, body)
-      (valid_admin_creation?(body) || !body.has_key?(:admin)) ? User.find(id).update!(body) : nil
+      authorized_user?(id, body) ? User.find(id).update!(body) : nil
     end
 
     def valid_admin_creation?(body)
       body.has_key?(:admin) && body.has_key?(:secret_key) && User.is_admin_key?(body[:secret_key])
+    end
+
+    def valid_key?(key)
+      !!User.find_by(secret_key: key)
     end
   end
 
