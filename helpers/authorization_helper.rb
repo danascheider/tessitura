a518dd587b606c92eb66395243e2933da2e3fbaf
danascheider <dana.scheider@gmail.com>
2014-07-26 16:48:18 -0700

    Finish defining tests, refactor module

--- helpers/authorization_helper.rb
+++ helpers/authorization_helper.rb
@@ -1,7 +1,34 @@
+# CREATE requests
+# - Always authorized unless request includes 'admin':true
+# - Always
+
 class Sinatra::Application
   module AuthorizationHelper
-    def authorized?(method, resource, id=nil, body=nil)
-      return true if User.is_admin_key?(body[:secret_key])
+    def authorized?(body)
+    end
+
+    def create_authorized?(body)
+      admin_approved?(body[:secret_key]) || !body[:admin]
+    end
+
+    def read_authorized?(id, body)
+      admin_approved?(body[:secret_key]) || user_match?(id, body[:secret_key])
+    end
+
+    def update_authorized?(id, body)
+      admin_approved?(body[:secret_key]) || (user_match?(id, body[:secret_key]) && !body[:admin])
+    end
+
+    def delete_authorized?(id, body)
+      admin_approved?(body[:secret_key]) || user_match?(id, body[:secret_key])
+    end
+
+    def admin_approved?(key)
+      User.find_by(secret_key: key) && User.find_by(secret_key: key).admin?
+    end
+
+    def user_match?(id, key)
+      User.find_by(secret_key: key).id == id
     end
   end
 
--- spec/helpers/authorization_helper_spec.rb
+++ spec/helpers/authorization_helper_spec.rb
@@ -12,13 +12,11 @@ describe Canto::AuthorizationHelper do
   describe 'admin-only actions' do 
     describe 'creating a new admin' do 
       it 'allows an admin to be created with an admin key' do 
-        body = { secret_key: @admin_key, email: 'user1@example.com', admin: true }
-        expect(authorized?('POST', 'users', nil, body)).to eql true
+        expect(create_authorized?({ secret_key: @admin_key, email: 'user1@example.com', admin: true })).to eql true
       end
 
       it 'doesn\'t allow an admin to be created without an admin key' do 
-        body = { email: 'user2@example.com', admin: true }
-        expect(authorized?('POST', 'users', nil, body)).to eql nil
+        expect(create_authorized?({ email: 'user2@example.com', admin: true })).to eql false
       end
     end
 
@@ -29,52 +27,12 @@ describe Canto::AuthorizationHelper do
 
       it 'allows admin to be set to true with admin key' do 
         body = { secret_key: @admin_key, admin: true }
-        expect(authorized?('PUT', 'users', @user.id, body)).to eql true
+        expect(update_authorized?(@user.id, body)).to eql true
       end
 
       it 'doesn\'t allow admin to be set to true without admin key' do 
         body = { secret_key: @user.secret_key, admin: true } 
-        expect(authorized?('PUT', 'users', @user.id, body)).to eql nil
-      end
-    end
-
-    describe 'viewing list of all users' do 
-      it 'authorizes viewing list of users with admin key' do 
-        expect(authorized?('GET', 'users', nil, { secret_key: @admin_key })).to eql true
-      end
-
-      it 'does not allow viewing list of users without admin key' do 
-        expect(authorized?('GET', 'users', nil, { secret_key: 'tunafish12' })).to eql nil
-      end
-    end
-
-    describe 'mass updates' do 
-      before(:each) do 
-        @user = FactoryGirl.create(:user)
-      end
-
-      it 'allows users to be updated with admin key' do 
-        body = { secret_key: @admin_key, 'fach' => 'mezzo-soprano' }
-        expect(authorized?('PUT', 'users', nil, body)).to eql true
-      end
-
-      it 'doesn\'t allow users to be updated without admin key' do 
-        body = { secret_key: @user.secret_key, 'fach' => 'mezzo-soprano' }
-        expect(authorized?('PUT', 'users', nil, body)).to eql false
-      end
-    end
-
-    describe 'mass deletion' do 
-      before(:each) do 
-        3.times { FactoryGirl.create(:user) }
-      end
-
-      it 'allows all users to be deleted with admin key' do 
-        expect(authorized?('DELETE', 'users', nil, { secret_key: @admin_key })).to eql true
-      end
-
-      it 'doesn\'t allow users to be deleted without admin key' do 
-        expect(authorized?('DELETE', 'users', nil, { secret_key: User.last.secret_key })).to eql nil
+        expect(update_authorized?(@user.id, body)).to eql false
       end
     end
   end
@@ -91,11 +49,11 @@ describe Canto::AuthorizationHelper do
       end
 
       it 'allows the user to view their own profile' do 
-        expect(authorized?('GET', 'users', @user1.id, @body)).to eql true
+        expect(read_authorized?(@user1.id, @body)).to eql true
       end
 
       it 'doesn\'t allow the user to view someone else\'s profile' do 
-        expect(authorized?('GET', 'users', @user2.id, @body)).to eql nil
+        expect(read_authorized?(@user2.id, @body)).to eql false
       end
     end
 
@@ -105,11 +63,11 @@ describe Canto::AuthorizationHelper do
       end
 
       it 'allows the user to edit their own profile' do 
-        expect(authorized?('PUT', 'users', @user1.id, @body)).to eql true
+        expect(update_authorized?(@user1.id, @body)).to eql true
       end
 
       it 'doesn\'t allow the user to edit someone else\'s profile' do 
-        expect(authorized?('PUT', 'users', @user2.id, @body)).to eql nil
+        expect(update_authorized?(@user2.id, @body)).to eql false
       end
     end
 
@@ -119,11 +77,11 @@ describe Canto::AuthorizationHelper do
       end
 
       it 'allows the user to delete their own profile' do 
-        expect(authorized?('DELETE', 'users', @user1.id, @body)).to eql true
+        expect(delete_authorized?(@user1.id, @body)).to eql true
       end
 
       it 'doesn\'t allow the user to delete someone else\'s profile' do 
-        expect(authorized?('DELETE', 'users', @user2.id, @body)).to eql nil
+        expect(delete_authorized?(@user2.id, @body)).to eql false
       end
     end
   end
