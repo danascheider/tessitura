2f309f5df55a6fb14644da603b6d1c62e441292b
danascheider <dana.scheider@gmail.com>
2014-07-28 08:56:05 -0700

    Add comment about next steps for this module

--- helpers/params_helper.rb
+++ helpers/params_helper.rb
@@ -1,5 +1,6 @@
 class Sinatra::Application
   module ParamsHelper
+    # FIX: This module currently doesn't accomplish what we need it to.
     ALLOWED_PARAMS = [ 'complete', 'status', 'priority', 'deadline', 'title', 'created', 'updated' ]
 
     def validate_params(params)
