5a8632d1808e2655490c84d341e83c7d454e1fc5
danascheider <dana.scheider@gmail.com>
2014-07-28 14:39:51 -0700

    Update some syntax & conventions

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,40 +2,40 @@ require 'spec_helper'
 
 describe Task do 
   describe 'attributes' do 
-    it { should respond_to(:title) }
-    it { should respond_to(:status) }
-    it { should respond_to(:position) }
-    it { should respond_to(:deadline) }
-    it { should respond_to(:description) }
-    it { should respond_to(:priority) }
+    it { is_expected.to respond_to(:title) }
+    it { is_expected.to respond_to(:status) }
+    it { is_expected.to respond_to(:position) }
+    it { is_expected.to respond_to(:deadline) }
+    it { is_expected.to respond_to(:description) }
+    it { is_expected.to respond_to(:priority) }
   end
 
   describe 'general methods' do 
-    it { should respond_to(:complete?) }
-    it { should respond_to(:incomplete?) }
-    it { should respond_to(:to_hash) } # to integrate with Sinatra-Backbone
-    it { should respond_to(:user) }
+    it { is_expected.to respond_to(:complete?) }
+    it { is_expected.to respond_to(:incomplete?) }
+    it { is_expected.to respond_to(:to_hash) } # to integrate with Sinatra-Backbone
+    it { is_expected.to respond_to(:user) }
   end
 
   describe 'acts_as_list methods' do 
-    it { should respond_to(:insert_at) }
-    it { should respond_to(:move_lower) }
-    it { should respond_to(:move_higher) }
-    it { should respond_to(:move_to_bottom) }
-    it { should respond_to(:move_to_top) }
-    it { should respond_to(:remove_from_list) }
-    it { should respond_to(:increment_position) }
-    it { should respond_to(:decrement_position) }
-    it { should respond_to(:set_list_position) }
-    it { should respond_to(:first?) }
-    it { should respond_to(:last?) }
-    it { should respond_to(:in_list?) }
-    it { should respond_to(:not_in_list?) }
-    it { should respond_to(:default_position?) }
-    it { should respond_to(:higher_item) }
-    it { should respond_to(:lower_item) }
-    it { should respond_to(:higher_items) }
-    it { should respond_to(:lower_items) }
+    it { is_expected.to respond_to(:insert_at) }
+    it { is_expected.to respond_to(:move_lower) }
+    it { is_expected.to respond_to(:move_higher) }
+    it { is_expected.to respond_to(:move_to_bottom) }
+    it { is_expected.to respond_to(:move_to_top) }
+    it { is_expected.to respond_to(:remove_from_list) }
+    it { is_expected.to respond_to(:increment_position) }
+    it { is_expected.to respond_to(:decrement_position) }
+    it { is_expected.to respond_to(:set_list_position) }
+    it { is_expected.to respond_to(:first?) }
+    it { is_expected.to respond_to(:last?) }
+    it { is_expected.to respond_to(:in_list?) }
+    it { is_expected.to respond_to(:not_in_list?) }
+    it { is_expected.to respond_to(:default_position?) }
+    it { is_expected.to respond_to(:higher_item) }
+    it { is_expected.to respond_to(:lower_item) }
+    it { is_expected.to respond_to(:higher_items) }
+    it { is_expected.to respond_to(:lower_items) }
   end
 
   describe 'public methods' do 
@@ -46,13 +46,13 @@ describe Task do
       @complete_task = Task.complete.order(:position).first
     end
 
-    describe 'self.first_complete' do 
+    describe '::first_complete' do 
       it 'returns the  first complete task' do 
         expect(Task.first_complete).to eql @complete_task
       end
     end
 
-    describe 'complete?' do 
+    describe '#complete?' do 
       it 'returns true when a task is complete' do 
         expect(@complete_task.complete?).to be true
       end
@@ -62,7 +62,7 @@ describe Task do
       end
     end
 
-    describe 'incomplete?' do 
+    describe '#incomplete?' do 
       it 'returns true when a task is incomplete' do 
         expect(@incomplete_task.incomplete?).to be true
       end
@@ -72,7 +72,7 @@ describe Task do
       end
     end
 
-    describe 'user' do 
+    describe '#user' do 
       it 'returns the user who owns the task list containing the task' do 
         expect(Task.first.user).to eql Task.first.task_list.user
       end
@@ -117,11 +117,20 @@ describe Task do
   describe 'default behavior' do 
     before(:each) do 
       @list = FactoryGirl.create(:task_list_with_tasks)
+      @list.create(:task, complete: true, position: 4)
       @task = FactoryGirl.create(:task, title: "New task", task_list_id: @list.id)
     end
 
-    it 'instantiates at position 1' do 
-      expect(@task.position).to eql 1
+    context 'when status is set to complete' do 
+      it 'instantiates as the first complete task' do 
+        #
+      end
+    end
+
+    context 'when status is not set to complete' do 
+      it 'instantiates at position 1' do 
+        expect(@task.position).to eql 1
+      end
     end
 
     it 'sets status to \'new\'' do
