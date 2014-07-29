08e94640e1c3d8253b568ca4edd8fe37c15adb9d
danascheider <dana.scheider@gmail.com>
2014-07-29 09:30:43 -0700

    Add new routes, remove old unneeded ones

--- canto.rb
+++ canto.rb
@@ -15,21 +15,11 @@ class Canto < Sinatra::Application
     validate_params(params)
   end
 
-  get '/tasks' do 
+  get '/users/:id/tasks' do |id|
+    # FIX: This will need a more robust error-handling approach - TBD
     begin_and_rescue(ActiveRecord::RecordNotFound, 404) do 
-      content_type :json
-      if params[:complete]
-        to_bool(params[:complete]) ? Task.complete.to_json : Task.incomplete.to_json
-      else
-        Task.all.to_json
-      end
-    end
-  end
-
-  post 'users/:id/tasks' do |id|
-    begin_and_rescue(ActiveRecord::RecordInvalid, 422) do 
-      if update_authorized?(id, body = request_body)
-      end
+      status(401) unless read_authorized?(id, request_body)
+      User.find(id).tasks.to_json
     end
   end
 
@@ -55,6 +45,20 @@ class Canto < Sinatra::Application
         return body({ 'secret_key' => User.last.secret_key }.to_json) && 201
       end
       401
+    end
+  end
+
+  get '/users' do 
+    begin_and_rescue(ActiveRecord::RecordNotFound, 404) do 
+      status(401) unless admin_approved?(request_body)
+      content_type :json
+      User.all.to_json
+    end
+  end
+
+  get '/users/:id' do |id|
+    begin_and_rescue(ActiveRecord::RecordNotFound, 404) do 
+      return User.find(id).to_json
     end
   end
 
