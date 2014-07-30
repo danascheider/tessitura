d230e995325663bcff0b56e5d2192636c116a7a5
danascheider <dana.scheider@gmail.com>
2014-07-30 09:29:12 -0700

    finish writing basic tests

--- spec/helpers/authorization_helper_spec.rb
+++ spec/helpers/authorization_helper_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper' 
 
 describe Canto::AuthorizationHelper do 
+  include Rack::Test::Methods
   include Canto::AuthorizationHelper
 
   describe '::authorized_credentials?' do 
@@ -24,6 +25,59 @@ describe Canto::AuthorizationHelper do
     context 'with invalid credentials' do 
       it 'returns false' do 
         expect(authorized_credentials?([@user.username, @user.password], @admin.id)).to eql false
+      end
+    end
+  end
+
+  describe '::authorized?' do 
+    context 'when the user is authorized' do 
+      context 'as a normal user' do 
+        it 'authorizes the request' do 
+          authorize @user.username, @user.password
+          get "/users/#{@user.id}"
+          expect(response_status).to eql 200
+        end
+      end
+
+      context 'as an admin' do 
+        it 'authorizes the request' do 
+          authorize @admin.username, @admin.password 
+          get "/users/#{@user.id}"
+          expect(response_status).to eql 200
+        end 
+      end
+    end
+
+    context 'when the user is not authorized' do 
+      context 'with no credentials' do 
+        it 'doesn\'t authorize the request' do
+          get "/users/#{@user.id}"
+          expect(response_status).to eql 401
+        end
+      end
+
+      context 'with invalid username' do 
+        it 'doesn\'t authorize the request' do 
+          authorize 'invalid', 'invalid'
+          get "/users/#{@user.id}"
+          expect(response_status).to eql 401
+        end
+      end
+
+      context 'with wrong password' do 
+        it 'doesn\'t authorize the request' do 
+          authorize @user.username, 'thisisabadpassword'
+          get "/users/#{@user.id}"
+          expect(response_status).to eql 401
+        end
+      end
+
+      context 'trying to view someone else\'s data' do 
+        it 'doesn\'t authorize the request' do 
+          authorize @user.username, @user.password
+          get "/users/#{@admin.id}"
+          expect(response_status).to eql 401
+        end
       end
     end
   end
