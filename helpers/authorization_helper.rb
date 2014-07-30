a1ef373c6313de1eca9028892d71d9c756636510
danascheider <dana.scheider@gmail.com>
2014-07-29 20:36:22 -0700

    Begin implementation of HTTP Basic

--- helpers/authorization_helper.rb
+++ helpers/authorization_helper.rb
@@ -1,41 +1,14 @@
-
-class Sinatra::Application
+class Sinatra::Application 
   module AuthorizationHelper
-    def create_authorized?(body=nil)
-      return false unless body
-      admin_approved?(body[:secret_key]) || !body[:admin]
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not Authorized\n"
     end
 
-    def read_authorized?(id, body=nil)
-      begin
-        admin_approved?(body[:secret_key]) || user_match?(id, body[:secret_key])
-      rescue(NoMethodError)
-        false
-      end
-    end
-
-    def update_authorized?(id, body=nil)
-      begin
-        admin_approved?(body[:secret_key]) || (user_match?(id, body[:secret_key]) && !body[:admin])
-      rescue(NoMethodError)
-        false
-      end
-    end
-
-    def delete_authorized?(id, body=nil)
-      begin
-        admin_approved?(body[:secret_key]) || user_match?(id, body[:secret_key])
-      rescue(NoMethodError)
-        false
-      end
-    end
-
-    def admin_approved?(key)
-      User.find_by(secret_key: key) && User.find_by(secret_key: key).admin? ? true : false
-    end
-
-    def user_match?(id, key)
-      User.find_by(secret_key: key).id == id
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
     end
   end
 
