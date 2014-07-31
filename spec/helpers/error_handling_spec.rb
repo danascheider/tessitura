1741863eba00310058396c25ec3087179bd9cdf5
danascheider <dana.scheider@gmail.com>
2014-07-31 10:25:58 -0700

    Write tests for all methods

--- spec/helpers/error_handling_spec.rb
+++ spec/helpers/error_handling_spec.rb
@@ -16,4 +16,33 @@ describe Canto::ErrorHandling do
       end
     end
   end
+
+  describe '::get_resource' do 
+    context 'when the resource exists' do 
+      it 'returns the resource' do 
+        user = FactoryGirl.create(:user)
+        expect(get_resource(User, user.id)).to eql user
+      end
+    end
+
+    context 'when the resource doesn\'t exist' do 
+      it 'returns nil' do 
+        expect(get_resource(User, 1000)).to eql nil
+      end
+    end
+  end
+
+  describe '::parse_json' do 
+    context 'when a valid JSON object is given' do 
+      it 'returns a hash' do 
+        expect(parse_json({"foo"=>"bar"}.to_json)).to eql({ 'foo' => 'bar' })
+      end
+    end
+
+    context 'when no valid JSON object is given' do 
+      it 'returns nil' do 
+        expect(parse_json("")).to eql nil
+      end
+    end
+  end
 end
