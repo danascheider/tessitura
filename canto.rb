51c1bd981d909637dcb9e3f0076b39bae871127d
danascheider <dana.scheider@gmail.com>
2014-08-03 13:35:29 -0700

    Incorporate return_json method

--- canto.rb
+++ canto.rb
@@ -16,11 +16,11 @@ class Canto < Sinatra::Application
   end
 
   authorize 'Admin' do |username, password|
-    @user = User.find_by(username: username)
-    password == @user.password && @user.admin?
+    user = User.find_by(username: username)
+    password == user.password && user.admin?
   end
 
-  before do 
+  before do
     @request_body = parse_json(request.body.read)
   end
 
@@ -40,7 +40,7 @@ class Canto < Sinatra::Application
   protect 'General' do 
     get '/users/:id' do |id|
       user_route_boilerplate(id)
-      @user ? @user.to_json : 404
+      return_json(@user) || 404
     end
   end
 
@@ -69,14 +69,14 @@ class Canto < Sinatra::Application
   protect 'General' do
     get '/users/:id/tasks' do |id|
       user_route_boilerplate(id)
-      get_resource(User, id) {|user| user.tasks.to_json } || 404
+      return_json(@user.tasks) || 404
     end
   end
 
   protect 'General' do
     get '/tasks/:id' do |id|
       task_route_boilerplate(id)
-      @task ? @task.to_json : 404
+      return_json(@task) || 404
     end
   end
 
@@ -102,7 +102,7 @@ class Canto < Sinatra::Application
 
   protect 'Admin' do 
     get '/admin/users' do 
-      User.all.to_json
+      return_json(User.all)
     end
   end
 end
