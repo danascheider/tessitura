810d01f16a42a4ddc5ecba8fd127dc3dbd08a658
danascheider <dana.scheider@gmail.com>
2014-08-03 20:16:33 -0700

    Rearrange methods in alphabetical order

--- helpers/authorization_helper.rb
+++ helpers/authorization_helper.rb
@@ -1,15 +1,5 @@
 class Sinatra::Application
   module AuthorizationHelper
-    def authorized?
-      @auth ||= Rack::Auth::Basic::Request.new(request.env)
-      @auth.provided? && @auth.basic? && valid_credentials?
-    end
-
-    def protected!
-      return if authorized?
-      access_denied
-    end
-
     def admin_access?
       current_user.admin?
     end
@@ -24,17 +14,22 @@ class Sinatra::Application
       halt 401, "Authorization Required\n"
     end
 
-    def valid_credentials?
-      begin
-        @auth.credentials.last == User.find_by(username: @auth.credentials.first).password
-      rescue
-        false
-      end
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && valid_credentials?
     end
 
-    def user_route_boilerplate(id)
-      return 404 unless (@user = get_resource(User, id))
-      halt 401 unless current_user.admin? || (current_user.id == id.to_i && !setting_admin?)
+    def current_user
+      User.find_by(username: @auth.credentials.first)
+    end
+
+    def protected!
+      return if authorized?
+      access_denied
+    end
+
+    def setting_admin?
+      @request_body && @request_body.has_key?('admin')
     end
 
     def task_route_boilerplate(id)
@@ -42,12 +37,17 @@ class Sinatra::Application
       halt 401 unless current_user.id == @task.user.id || current_user.admin?
     end
 
-    def setting_admin?
-      @request_body && @request_body.has_key?('admin')
+    def user_route_boilerplate(id)
+      return 404 unless (@user = get_resource(User, id))
+      halt 401 unless current_user.admin? || (current_user.id == id.to_i && !setting_admin?)
     end
 
-    def current_user
-      User.find_by(username: @auth.credentials.first)
+    def valid_credentials?
+      begin
+        @auth.credentials.last == User.find_by(username: @auth.credentials.first).password
+      rescue
+        false
+      end
     end
   end
 
