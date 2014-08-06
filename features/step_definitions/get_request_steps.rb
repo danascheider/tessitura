e938565cf47d7f12f7c52ba4013ec46934d91fd8
danascheider <dana.scheider@gmail.com>
2014-08-05 17:23:23 -0700

    Finish reorganizing GET reques tsteps

--- features/step_definitions/get_request_steps.rb
+++ features/step_definitions/get_request_steps.rb
@@ -16,16 +16,31 @@ When(/^the client submits a GET request to \/tasks\/(\d+) with the (\d+)(?:[a-z]
   make_request('GET', "/tasks/#{task_id}")
 end
 
+# View User Profile
+# =================
+When(/^the client submits a GET request to \/users\/(\d+) with the (\d+)(?:[a-z]{2}) user's credentials$/) do |req_id, actual_id|
+  @user = get_resource(User, req_id)
+  @current = get_resource(User, actual_id)
+  authorize @current.username, @current.password
+  make_request('GET', "/users/#{req_id}")
+end
+
 # View All Users
 # ==============
-When(/^the client submits a GET request to \/admin\/users with admin credentials$/) do
-  @admin = User.admin.first 
-  authorize @admin.username, @admin.password
+When(/^the client submits a GET request to \/admin\/users with the (\d+)(?:[a-z]{2}) user's credentials$/) do |id|
+  @current = get_resource(User, id) 
+  authorize @current.username, @current.password
   make_request('GET', '/admin/users')
 end
 
-# General
-# =======
+# Failed
+# ======
 When(/^the client submits a GET request to (.*) with no credentials$/) do |path|
   make_request('GET', path)
+end
+
+When(/^the client submits a GET request to \/users with the (\d+)(?:[a-z]{2}) user's credentials$/) do |id|
+  @current = get_resource(User, id)
+  authorize @current.username, @current.password
+  make_request('GET', '/users')
 end
