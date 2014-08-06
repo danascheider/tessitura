6df17bbe3ae1072170bf6244f8cda4b8be4789fe
danascheider <dana.scheider@gmail.com>
2014-08-05 22:07:00 -0700

    Refactor for brevity, rename rest_steps to response_steps

---
+++ features/step_definitions/response_steps.rb
@@ -0,0 +1,54 @@
+Then(/^the JSON response should include all the (\d+)nd user's tasks$/) do |id|
+  expect(response_body).to eql User.find(id).tasks.to_json
+end
+
+Then(/^the JSON response should include all the users$/) do 
+  expect(response_body).to eql User.all.to_json
+end
+
+Then(/^the JSON response should include only the (in)?complete task(?:s)$/) do |scope|
+  tasks = scope ? Task.incomplete : Task.complete
+  response_body.should eql tasks.to_json
+end
+
+Then(/^the JSON response should include (?:only )?the (\d+)(?:[a-z]{2}) task$/) do |id|
+  response_body.should === get_resource(Task, id).to_json
+end
+
+Then(/^the JSON response should include the (first|last) task$/) do |order|
+  task = order == 'first' ? Task.first : Task.last
+  expect(response_body).to eql task.to_json
+end
+
+Then(/^the JSON response should include the (\d+)(?:[a-z]{2}) user's profile information$/) do |id|
+  expect(response_body).to eql User.find(id).to_json
+end
+
+Then(/^the response should not include any data$/) do 
+  ok_values = [nil, '', 'null', false, "Authorization Required\n"]
+  expect(ok_values).to include response_body
+end
+
+Then(/^the response should indicate the (?:.*) was (not )?saved successfully$/) do |negation|
+  expect(response_status).to eql negation ? 422 : 201
+end
+
+Then(/^the response should indicate the (?:.*) was (not )?updated successfully$/) do |negation|
+  expect(response_status).to eql negation ? 422 : 200
+end
+
+Then(/^the response should indicate the request was unauthorized$/) do
+  expect(response_status).to eql 401
+end
+
+Then(/^the response should return status (\d{3})$/) do |status|
+  expect(response_status).to eql status
+end
+
+Then(/^the response should indicate the (?:.*) was deleted successfully$/) do
+  expect(response_status).to eql 204
+end
+
+Then(/^the response should indicate the (?:.*) was not found$/) do
+  expect(response_status).to eql 404
+end
