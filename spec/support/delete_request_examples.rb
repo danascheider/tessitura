2cb2ecee03e3dcaa7800a7f5e4e64cc109e2f9ad
danascheider <dana.scheider@gmail.com>
2014-08-07 16:06:49 -0700

    Move some stuff to before filters

--- spec/support/delete_request_examples.rb
+++ spec/support/delete_request_examples.rb
@@ -1,13 +1,15 @@
 shared_examples 'an authorized DELETE request' do 
+  before(:each) do 
+    authorize_with agent
+  end
+
   context 'when the resource exists' do 
     it 'deletes the resource' do 
       expect_any_instance_of(model).to receive(:destroy!)
-      authorize_with agent
       make_request('DELETE', path)
     end
 
     it 'returns status 204' do 
-      authorize_with agent
       make_request('DELETE', path)
       expect(response_status).to eql 204
     end
@@ -16,12 +18,10 @@ shared_examples 'an authorized DELETE request' do
   context 'when the resource doesn\'t exist' do 
     it 'doesn\'t delete anything' do 
       expect_any_instance_of(model).not_to receive(:destroy!)
-      authorize_with agent
       make_request('DELETE', nonexistent_resource_path)
     end
 
     it 'returns status 404' do 
-      authorize_with agent
       make_request('DELETE', nonexistent_resource_path)
       expect(response_status).to eql 404
     end
@@ -29,14 +29,16 @@ shared_examples 'an authorized DELETE request' do
 end
 
 shared_examples 'an unauthorized DELETE request' do 
+  before(:each) do 
+    authorize_with agent
+  end
+
   it 'doesn\'t delete the resource' do 
     expect_any_instance_of(model).not_to receive(:destroy!)
-    authorize_with agent
     make_request('DELETE', path)
   end
 
   it 'returns status 401' do 
-    authorize_with agent
     make_request('DELETE', path)
     expect(response_status).to eql 401
   end
