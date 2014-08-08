a6b7a7518f0d9139bbac8bcc63dce30d956d4816
danascheider <dana.scheider@gmail.com>
2014-08-08 16:32:00 -0700

    Flesh out tests for AuthorizationHelper module

--- spec/helpers/authorization_helper_spec.rb
+++ spec/helpers/authorization_helper_spec.rb
@@ -8,18 +8,20 @@ require 'spec_helper'
 
 describe Canto::AuthorizationHelper do 
   include Rack::Test::Methods
+  include Sinatra::Helpers
   include Canto::AuthorizationHelper
+
+  let(:admin) { FactoryGirl.create(:admin) }
+  let(:user) { FactoryGirl.create(:user_with_task_lists) }
+  let(:task) { user.tasks.first } 
+  let(:admin_credentials) { Base64.encode64("#{admin.username}:#{admin.password}") }
+  let(:user_credentials) { Base64.encode64("#{user.username}:#{user.password}") }
+  let(:bogus_credentials) { Base64.encode64('foo:bar') }
   
   before(:each) do 
-    @admin = FactoryGirl.create(:admin)
-    @user = FactoryGirl.create(:user_with_task_lists)
-    @task = Task.first
-
-    @admin_credentials = Base64.encode64("#{@admin.username}:#{@admin.password}")
-    @user_credentials = Base64.encode64("#{@user.username}:#{@user.password}")
-
-    @ENV1 = { "HTTP_AUTHORIZATION" => "Basic #{@admin_credentials}" }
-    @ENV2 = { "HTTP_AUTHORIZATION" => "Basic #{@user_credentials}" }
+    @ENV1 = { "HTTP_AUTHORIZATION" => "Basic #{admin_credentials}" }
+    @ENV2 = { "HTTP_AUTHORIZATION" => "Basic #{user_credentials}" }
+    @ENV3 = { "HTTP_AUTHORIZATION" => "Basic #{bogus_credentials}" }
   end
 
   describe '::admin_access?' do 
@@ -42,18 +44,156 @@ describe Canto::AuthorizationHelper do
   # otherwise, they return status code 401 and WWW-Authorization headers
 
   describe '::admin_only!' do 
-    pending
-    context 'when logged-in user is an admin' do 
-      it 'returns nil' do 
+    context 'when user is an admin' do 
+      it 'allows access to the resource' do 
         @auth = Rack::Auth::Basic::Request.new(@ENV1)
         expect(admin_only!).to eql nil
       end
     end
 
     context 'when logged-in user is not an admin' do 
+      it 'doesn\'t allow access to the resource' do 
+        @auth = Rack::Auth::Basic::Request.new(@ENV2)
+        make_request('GET', '/test/admin_only')
+        expect(response_body).to eql "Authorization Required\n"
+      end
     end
 
     context 'when not logged in' do 
+      it 'doesn\'t allow access to the resource' do 
+        make_request('GET', '/test/admin_only')
+        expect(response_body).to eql "Authorization Required\n"
+      end
+    end
+  end
+
+  describe '::access_denied' do 
+    before(:each) do 
+      make_request('GET', '/test/access_denied')
+    end
+
+    it 'sets the WWW-Authenticate header' do 
+      expect(last_response.headers['WWW-Authenticate']).to eql 'Basic realm="Restricted Area"'
+    end
+
+    it 'returns status 401' do 
+      expect(response_status).to eql 401
+    end
+
+    it 'halts the request' do 
+      expect(response_body).not_to eql({ 'message' => 'Hello world' }.to_json)
+    end
+  end
+
+  describe '::authorized?' do 
+    context 'with proper credentials' do 
+      it 'returns true' do 
+        @auth = Rack::Auth::Basic::Request.new(@ENV2)
+        expect(authorized?).to be_truthy
+      end
+    end
+
+    context 'with bad credentials' do 
+      it 'returns false' do 
+        @auth = Rack::Auth::Basic::Request.new(@ENV3)
+        expect(authorized?).to be_falsey
+      end
+    end
+
+    context 'with no credentials' do 
+      it 'returns false' do 
+        @auth = Rack::Auth::Basic::Request.new({})
+        expect(authorized?).to be_falsey
+      end
+    end
+  end
+
+  describe '::authorized_for_resource?' do 
+    context 'when the logged-in user is an admin' do 
+      it 'returns true' do 
+        allow(@auth).to receive(:credentials).and_return([admin.username, admin.password])
+        expect(authorized_for_resource?(user.id)).to be_truthy
+      end
+    end
+
+    context 'when the logged-in user is the owner of the resource' do 
+      it 'returns true' do 
+        allow(@auth).to receive(:credentials).and_return([user.username, user.password])
+        expect(authorized_for_resource?(user.id)).to be_truthy
+      end
+    end
+
+    context 'when the logged-in user is not the owner of the resource' do
+      it 'returns false' do 
+        allow(@auth).to receive(:credentials).and_return([user.username, user.password])
+        expect(authorized_for_resource?(admin.id)).to be_falsey
+      end
+
+      it 'doesn\'t let a user set admin' do
+        allow(@auth).to receive(:credentials).and_return([user.username, user.password])
+        stub(:setting_admin?).and_return(true)
+        expect(authorized_for_resource?(user.id)).to be_falsey
+      end
+    end
+  end
+
+  describe '::current_user' do 
+    it 'returns the logged-in user' do 
+      allow(@auth).to receive(:credentials).and_return([user.username, user.password])
+      expect(current_user).to eql user
+    end
+  end
+
+  describe '::protect' do  
+    context 'when the user doesn\'t exist' do 
+      it 'returns 404' do 
+        allow(User).to receive(:exists?).and_return(false)
+        @id = 1000000
+        expect(protect(User)).to eql 404
+      end
+    end
+
+    context 'when the user doesn\'t have access' do 
+      before(:each) do 
+        allow_any_instance_of(Canto).to receive(:authorized?).and_return(false)
+      end
+
+      it 'returns 401' do 
+        make_request('GET', "/test/users/#{admin.id}")
+        expect(response_status).to eql 401
+      end
+
+      it 'calls ::access_denied' do 
+        expect_any_instance_of(Canto).to receive(:access_denied)
+        make_request('GET', "/test/users/#{admin.id}")
+      end
+    end
+
+    context 'when the user is authorized' do 
+      it 'allows access to the route'
+    end
+  end
+
+  describe '::setting_admin' do 
+    context 'when the user is attempting to set admin' do 
+      it 'returns true' do 
+        make_request('POST', '/test/setting_admin', { 'admin' => true }.to_json)
+        expect(response_body).to eql([true].to_json)
+      end
+    end
+
+    context 'when the user is not attempting to set admin' do 
+      it 'returns false' do 
+        make_request('POST', '/test/setting_admin', { 'fach' => 'dramatic coloratura' }.to_json)
+        expect(response_body).to eql([false].to_json)
+      end
+    end
+
+    context 'when there is no request body' do 
+      it 'returns false' do 
+        make_request('POST', '/test/setting_admin')
+        expect(response_body).to eql([nil].to_json)
+      end
     end
   end
 end
---
+++ spec/support/authorization_helper_support.rb
@@ -0,0 +1,20 @@
+class Canto < Sinatra::Application
+  get '/test/access_denied' do 
+    access_denied
+    { 'message' => 'Hello world' }.to_json
+  end
+
+  get '/test/current_user' do 
+    [ current_user ].to_json
+  end
+
+  post '/test/setting_admin' do 
+    [ setting_admin? ].to_json
+  end
+
+  get '/test/users/id' do 
+    @id = id
+    protect(User)
+    { 'message' => 'Hello world' }.to_json
+  end
+end
