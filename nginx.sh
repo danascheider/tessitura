aea9a445030c2301f420c33ba1dd2c404e7194bb
Dana Scheider <dana.scheider@gmail.com>
2014-08-12 15:20:48 -0700

    Symbolic link to nginx binary & run

--- nginx.sh
+++ nginx.sh
@@ -13,6 +13,12 @@ cd src
 wget http://nginx.org/download/nginx-1.7.4.tar.gz
 tar -zxf nginx-1.7.4.tar.gz
 cd nginx-1.7.4
-./configure --user=www-data --group=www-data --with-http_ssl_module --with-http_realip_module --with-ipv6 --with-debug
+./configure --user=root --group=www-data --with-http_ssl_module --with-http_realip_module --with-ipv6 --with-debug
 make 
 make install
+
+# Symbolic link for the nginx binary goes in /usr/bin so 
+# nginx command can be used
+ln -s /usr/local/nginx/sbin/nginx /usr/bin/nginx
+
+nginx
