dcced44471c22ce93254e45b249a6e5e1995f333
Dana Scheider <dana.scheider@gmail.com>
2014-08-13 14:13:04 -0700

    Define filter_resources example

--- spec/helpers/filter_helper_spec.rb
+++ spec/helpers/filter_helper_spec.rb
@@ -3,17 +3,24 @@ require 'spec_helper'
 describe Canto::FilterHelper do 
   include Canto::FilterHelper
 
+  let(:user) { FactoryGirl.create(:user_with_task_lists) }
+
+  before(:each) do 
+    @list, @task = user.task_lists.first, user.task_lists.first.tasks.first
+    @task.update!(priority: 'high')
+    @hash = {'user' => @list.owner.id, 'resource' => 'tasks', 'filters' => {'priority' => 'high'}}
+  end
+
   describe '::get_filtered' do 
-    let(:user) { FactoryGirl.create(:user_with_task_lists) }
-
-    before(:each) do 
-      @list, @task = user.task_lists.first, user.task_lists.first.tasks.first
-      @task.update!(priority: 'high')
-      @hash = {'user' => @list.owner.id, 'resource' => 'tasks', 'filters' => {'priority' => 'high'}}
-    end
-
     it 'returns the correct tasks' do 
       expect(get_filtered(@hash)).to eq [@task]
+    end
+  end
+
+  describe '::filter_resources' do 
+    it 'returns relevant resources as JSON' do 
+      expected = [ @task.to_hash ].to_json
+      expect(filter_resources(@hash)).to eql expected
     end
   end
 end
