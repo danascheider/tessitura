7821dd47b2842ba1ce0b2decc34d29a865b333f2
Dana Scheider <dana.scheider@gmail.com>
2014-08-13 12:32:31 -0700

    Define 'user is logged in'

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -1,6 +1,10 @@
 Given(/^the (\d+)(?:[a-z]{2}) user has the following tasks:$/) do |id, tasks|
-  @user = User.find(id)
   tasks.hashes.each do |hash|
-    @user.default_task_list.tasks.create(hash)
+    User.find(id).default_task_list.tasks.create(hash)
   end
+end
+
+Given(/^the (\d+)(?:[a-z]{2}) user is logged in$/) do |id|
+  @user = User.find(id)
+  authorize_with @user
 end
