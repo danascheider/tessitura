480f9aeeddc8bf814df2e6f17b59758ee5632196
Dana Scheider <dana.scheider@gmail.com>
2014-08-13 14:29:55 -0700

    Spec file for filter routes

---
+++ spec/routes/filter_routes_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Canto do 
+  include Rack::Test::Methods
+
+  let(:admin) { FactoryGirl.create(:admin) }
+  let(:user) { FactoryGirl.create(:user_with_task_lists) }
+  let(:list) { FactoryGirl.create(:task_list_with_complete_and_incomplete_tasks, user_id: user.id) }
+
+  describe 'filtering tasks' do 
+    before(:each) do 
+      @list = list 
+      make_request('POST', '/filters', { 'user' => @list.user.id, 'resource' => 'tasks', 'filters' => { 'status' => 'complete' }}.to_json)
+    end
+
+    it 'returns the tasks in JSON format' do 
+      expected = Task.where(status: 'complete').to_a.map! {|task| task.to_hash }
+      expect(response_body).to eql expected.to_json
+    end
+
+    it 'returns status 200' do 
+      expect(response_status).to eql 200
+    end
+  end
+end
