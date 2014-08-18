6e0156221a085dba093a88cb44251256bd57226f
Dana Scheider <dana.scheider@gmail.com>
2014-08-18 14:53:14 -0700

    Extract #two_sided_time_range method

--- models/task_filter.rb
+++ models/task_filter.rb
@@ -19,19 +19,6 @@ class TaskFilter
   end
 
   protected
-    def parse_conditions!(conditions)
-      conditions.each do |key, value|
-        if value.length == 1
-          if value.has_key? :on
-            conditions[key] = parse_datetime(value[:on])
-          else
-            return one_sided_time_range(conditions)
-          end
-        else
-          conditions[key] = (parse_datetime(value[:after])..parse_datetime(value[:before]))
-        end
-      end
-    end
 
     # The hash that is passed into the #one_sided_time_range method is the hash within
     # filters designating a date or time, for example:
@@ -42,9 +29,23 @@ class TaskFilter
       string_condition(attribute.keys.first,attr_name,attribute.values.first)
     end
 
+    def parse_conditions!(conditions)
+      conditions.each do |key, value|
+        if value.length == 2 || value.has_key?(:on)
+          conditions[key] = two_sided_time_range(key => value)
+        else
+          return one_sided_time_range(key => value)
+        end
+      end
+    end
+
     def parse_datetime(date)
       year, month, day = date[:year], date[:month], date[:day]
       Time.utc(year, month, day)
+    end
+
+    def sanitize!(conditions)
+      conditions = conditions.reject {|key, value| !TASK_ATTRIBUTES.include?(key) }
     end
 
     # #string_condition takes the following arguments:
@@ -56,7 +57,19 @@ class TaskFilter
       condition == :before ? ["#{attr_name.to_s} < ?", parse_datetime(date_hash)] : ["#{attr_name.to_s} > ?", parse_datetime(date_hash)]
     end
 
-    def sanitize!(conditions)
-      conditions = conditions.reject {|key, value| !TASK_ATTRIBUTES.include?(key) }
+    # Like the #one_sided_time_range method, #two_sided_time_range takes a hash including the
+    # name of the attribute in question:
+    # => { created_at: { before: { year: 2014, month: 8, day: 31 }, after: { year: 2014, month: 8, day: 1} } }
+
+    # This method also handles time conditions with the :on key:
+    # => { created_at: { on: { year: 2014, month: 8, day: 12 } } } 
+
+    def two_sided_time_range(hash)
+      attribute = hash[attr_name = hash.keys.first]
+      if attribute.has_key? :on
+        hash[attr_name] = parse_datetime(attribute[:on])
+      else
+        hash[attr_name] = (parse_datetime(attribute[:after])..parse_datetime(attribute[:before]))
+      end
     end
 end
