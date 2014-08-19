0c67a4a25b4ccab8ed15e5003edf13d4d63aa4fe
Dana Scheider <dana.scheider@gmail.com>
2014-08-19 15:28:04 -0700

    Replace examples with shared examples from 'a filter'

--- spec/models/task_filter_spec.rb
+++ spec/models/task_filter_spec.rb
@@ -22,57 +22,28 @@ describe TaskFilter do
 
     context 'with simple time conditions' do 
       let(:conditions) { { deadline: { on: { year: 2014, month: 9, day: 3 }}}}
-      let(:filter) { TaskFilter.new(conditions, @list.owner_id) }
 
-      it 'returns an ActiveRecord::Relation' do 
-        expect(filter.filter).to be_an(ActiveRecord::Relation)
-      end
-
-      it 'returns the task with the given deadline' do 
-        expect(filter.filter.to_a).to eql [@task]
+      it_behaves_like 'a filter' do 
+        let(:included_tasks) { Task.where(deadline: Time.utc(2014, 9, 3)) }
+        let(:excluded_tasks) { Task.where.not(deadline: Time.utc(2014, 9, 3)) }
       end
     end
 
     context 'with one-sided :before interval' do 
       let(:conditions) { { deadline: { before: { year: 2014, month: 9, day: 3 } } } }
-      let(:filter) { TaskFilter.new(conditions, @list.owner_id) }
-      let(:filtered_tasks) { filter.filter }
 
-      it 'returns an ActiveRecord::Relation' do 
-        expect(filtered_tasks).to be_an(ActiveRecord::Relation)
-      end
-
-      it 'returns the tasks with earlier deadlines' do 
-        expect(filtered_tasks.to_a).to eql Task.where('deadline < ?', Time.utc(2014,9,3)).to_a
-      end
-
-      it 'excludes the tasks with equal or later deadlines' do 
-        excluded = Task.where('deadline > ?', Time.utc(2014,9,2))
-        expect(filtered_tasks.to_a & excluded.to_a).to eql []
-      end
-
-      it 'excludes tasks with no deadline' do 
-        @list.tasks.create(title: 'Make dentist appointment')
-        expect(filtered_tasks.to_a).not_to include(Task.find_by(title: 'Make dentist appointment'))
+      it_behaves_like 'a filter' do 
+        let(:included_tasks) { Task.where(['deadline < ?', Time.utc(2014,9,3)]) }
+        let(:excluded_tasks) { Task.where.not(['deadline < ?', Time.utc(2014,9,3)]) }
       end
     end
 
     context 'with one-sided :after interval' do 
       let(:conditions) { { deadline: { after: { year: 2014, month: 9, day: 2 } } } }
-      let(:filter) { TaskFilter.new(conditions, @list.owner_id) }
-      let(:filtered_tasks) { filter.filter }
 
-      it 'returns an ActiveRecord::Relation' do 
-        expect(filtered_tasks).to be_an(ActiveRecord::Relation)
-      end
-
-      it 'returns the tasks with later deadlines' do 
-        expect(filtered_tasks.to_a).to eql Task.where('deadline > ?', Time.utc(2014,9,2)).to_a
-      end
-
-      it 'excludes tasks with equal or earlier deadlines' do 
-        excluded = Task.where('deadline < ?', Time.utc(2014,9,3)).to_a
-        expect(filtered_tasks.to_a & excluded).to eql []
+      it_behaves_like 'a filter' do 
+        let(:included_tasks) { Task.where(['deadline > ?', Time.utc(2014,9,2)]) }
+        let(:excluded_tasks) { Task.where.not(['deadline > ?', Time.utc(2014,9,2)]) }
       end
     end
   end
