9104bb1269e253105cff2fffd313377e76e576aa
Dana Scheider <dana.scheider@gmail.com>
2014-08-28 12:26:25 -0700

    Remove to_bool method - unused

--- helpers/general_helper_methods.rb
+++ helpers/general_helper_methods.rb
@@ -1,9 +1,5 @@
 module Sinatra
   module GeneralHelperMethods
-    def to_bool(string)
-      string == 'false' || string == 'nil' ? false : true
-    end
-
     def return_json(obj)
       nil_values = [ 'null', '', {}, nil, [] ]
       obj.to_json unless nil_values.include? obj
--- spec/helpers/general_helper_methods_spec.rb
+++ spec/helpers/general_helper_methods_spec.rb
@@ -3,26 +3,6 @@ require 'spec_helper'
 describe Sinatra::GeneralHelperMethods do 
   include Sinatra::GeneralHelperMethods
 
-  describe '::to_bool' do 
-    context 'when false' do 
-      it 'returns false' do 
-        expect(to_bool('false')).to eql false
-      end
-    end
-
-    context 'when nil' do 
-      it 'returns false' do 
-        expect(to_bool('nil')).to eql false
-      end
-    end
-
-    context 'when true' do 
-      it 'returns true' do 
-        expect(to_bool('true')).to eql true
-      end
-    end
-  end
-
   describe '::return_json' do 
     context 'hash' do 
       it 'returns JSON' do 
