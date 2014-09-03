65cb7ce0e14378d8f0231deae13d3b4c5b3beeae
Dana Scheider <dana.scheider@gmail.com>
2014-09-03 13:50:42 -0700

    Include the migration extension

--- db/sequel_config.rb
+++ db/sequel_config.rb
@@ -3,6 +3,8 @@ require 'sequel'
 DB_DIR = File.expand_path('..', __FILE__)
 environment = ENV['RACK_ENV'] || 'development'
 
+Sequel.extension :migration
+
 DB = Sequel.sqlite("./#{environment}.sqlite3")
 
 DB.create_table :users do 
