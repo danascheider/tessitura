0965e4c9673f5368e471f7ed86438269a091403e
Dana Scheider <dana.scheider@gmail.com>
2014-09-03 14:19:51 -0700

    Delete hard-coded schema

--- db/sequel_config.rb
+++ db/sequel_config.rb
@@ -6,12 +6,3 @@ environment = ENV['RACK_ENV'] || 'development'
 Sequel.extension :migration
 
 DB = Sequel.sqlite("./#{environment}.sqlite3")
-
-DB.create_table :users do 
-  primary_key :id
-  String :username
-  String :password
-  String :email
-  String :first_name
-  String :last_name
-end
