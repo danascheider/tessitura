62c68b771a1ea7070c6bb103cc9e23757bff8307
Dana Scheider <dana.scheider@gmail.com>
2014-09-14 16:17:46 -0700

    Modify to talk to new databases

--- config/settings.rb
+++ config/settings.rb
@@ -2,15 +2,16 @@ require_all File.expand_path('../../helpers',__FILE__)
 
 class Canto < Sinatra::Base
   ENVIRONMENT = ENV['RACK_ENV'] || 'development'
+  ROOT_PASSWORD = 'vagrant'
 
   set :root, File.dirname(__FILE__)
   set :app_file, __FILE__
-  set :database_file, 'config/database.yml'
-  set :database, "/db/#{ENVIRONMENT}.sqlite3"
+  set :database, "mysql2://root:#{ROOT_PASSWORD}@127.0.0.1:3306/#{ENVIRONMENT}"
   set :data, ''
 
-  Sequel::Model.db = database
-  DB = Sequel.sqlite("sqlite:/#{database}")
+  DB = Sequel.connect(database)
+
+  Sequel::Model.plugin :timestamps
 
   helpers Sinatra::AuthorizationHelper 
   helpers Sinatra::ErrorHandling
