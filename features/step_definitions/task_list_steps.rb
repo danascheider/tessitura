770bd0c4c9c62762aada3067cb2c499a03671074
Dana Scheider <dana.scheider@gmail.com>
2014-09-21 12:00:00 -0700

    Update syntax to work better with Sequel

--- features/step_definitions/task_list_steps.rb
+++ features/step_definitions/task_list_steps.rb
@@ -1,3 +1,3 @@
 Then(/^a new task should be created on the (\d+)(?:[a-z]{2}) user\'s task list$/) do |uid|
-  expect(User.find(uid).tasks.count - @user_task_count).to eql 1
+  expect(User[uid].tasks.count - @user_task_count).to eql 1
 end
