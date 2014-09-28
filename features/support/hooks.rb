8015d72150ab69a1d54336f331df5b97cf3d39b7
Dana Scheider <dana.scheider@gmail.com>
2014-09-28 14:19:40 -0700

    Define @user_count in before hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,6 +3,7 @@ Before('@users') do
   FactoryGirl.create(:admin, id: 1, email: 'admin@example.com', username: 'abcd1234', password: 'abcde12345')
   FactoryGirl.create(:user, id: 2, email: 'user2@example.com', username: 'bcde2345', password: 'bcdef23456')
   FactoryGirl.create(:user, id: 3, email: 'user3@example.com', username: 'cdef3456', password: 'cdefg34567')
+  @user_count = User.count
 end
 
 # # This seems like a smell, but the fact is that sometimes I just need
