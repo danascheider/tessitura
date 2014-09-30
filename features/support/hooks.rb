4e08fe78d1095aaa8e26289704813d06a0f72836
Dana Scheider <dana.scheider@gmail.com>
2014-09-30 10:37:47 -0700

    Make all task-related features pass

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,22 +6,22 @@ Before do
   @user_count = User.count
 end
 
-# # This seems like a smell, but the fact is that sometimes I just need
-# # to know exactly what the tasks' IDs are.
-# Before('@tasks') do |scenario|
-#   list_id_1 = User[1].default_task_list.id
-#   list_id_2 = User[2].default_task_list.id
-#   list_id_3 = User[3].default_task_list.id
+# This seems like a smell, but the fact is that sometimes I just need
+# to know exactly what the tasks' IDs are.
+Before('@tasks') do |scenario|
+  list_id_1 = User[1].default_task_list.id
+  list_id_2 = User[2].default_task_list.id
+  list_id_3 = User[3].default_task_list.id
 
-#   FactoryGirl.create(:task, task_list_id: list_id_1, id: 1)
-#   FactoryGirl.create(:task, task_list_id: list_id_1, id: 2)
-#   FactoryGirl.create(:task, task_list_id: list_id_1, id: 3)
+  FactoryGirl.create(:task, task_list_id: list_id_1, id: 1)
+  FactoryGirl.create(:task, task_list_id: list_id_1, id: 2)
+  FactoryGirl.create(:task, task_list_id: list_id_1, id: 3)
 
-#   FactoryGirl.create(:task, task_list_id: list_id_2, id: 4)
-#   FactoryGirl.create(:task, task_list_id: list_id_2, id: 5)
-#   FactoryGirl.create(:task, task_list_id: list_id_2, id: 6)
+  FactoryGirl.create(:task, task_list_id: list_id_2, id: 4)
+  FactoryGirl.create(:task, task_list_id: list_id_2, id: 5)
+  FactoryGirl.create(:task, task_list_id: list_id_2, id: 6)
 
-#   FactoryGirl.create(:task, task_list_id: list_id_3, id: 7)
-#   FactoryGirl.create(:task, task_list_id: list_id_3, id: 8)
-#   FactoryGirl.create(:task, task_list_id: list_id_3, id: 9)
-# end
+  FactoryGirl.create(:task, task_list_id: list_id_3, id: 7)
+  FactoryGirl.create(:task, task_list_id: list_id_3, id: 8)
+  FactoryGirl.create(:task, task_list_id: list_id_3, id: 9)
+end
