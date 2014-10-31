8b4a96bfb546797baf73404e8d333ac6a78aa9af
Dana Scheider <dana.scheider@gmail.com>
2014-10-31 11:04:53 -0700

    Change #setting_admin? method to check for :admin and 'admin' keys both

--- helpers/authorization_helper.rb
+++ helpers/authorization_helper.rb
@@ -39,7 +39,7 @@ module Sinatra
     end
 
     def setting_admin?
-      @request_body.try(:has_key?, :admin)
+      @request_body.try(:has_key?, :admin) || @request_body.try(:has_key?, 'admin')
     end
 
     def valid_credentials?
