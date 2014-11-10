40bf679cc1419a89d4b2c677231f43c3b64ff12d
Dana Scheider <dana.scheider@gmail.com>
2014-11-09 16:01:43 -0800

    Move statements that were apparently causing Sequel validation failures during testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,18 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 ]
 SimpleCov.start if ENV["COVERAGE"]
 
+ENV['RACK_ENV'] = 'test'
+DB_PASSWD = 'hunter2'
+DB_PATH = ENV['TRAVIS'] ? "mysql2://travis@127.0.0.1:3306/test" : "mysql2://canto:#{DB_PASSWD}@127.0.0.1:3306/#{ENV['RACK_ENV']}"
+
+db_info = {host: '127.0.0.1', port: 3306}
+
+if ENV['TRAVIS']
+  db_info[:username], db_info[:database] = 'travis', 'test'
+else
+  db_info[:username], db_info[:password], db_info[:database] = 'canto', DB_PASSWD, ENV['RACK_ENV']
+end
+
 support_path = File.expand_path('../../features/support', __FILE__)
 app_path = File.expand_path('../..', __FILE__)
 
@@ -21,18 +33,6 @@ require_relative support_path + '/factories'
 require_relative support_path + '/helpers'
 
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f }
-
-ENV['RACK_ENV'] = 'test'
-DB_PASSWD = 'hunter2'
-DB_PATH = ENV['TRAVIS'] ? "mysql2://travis@127.0.0.1:3306/test" : "mysql2://canto:#{DB_PASSWD}@127.0.0.1:3306/#{ENV['RACK_ENV']}"
-
-db_info = {host: '127.0.0.1', port: 3306}
-
-if ENV['TRAVIS']
-  db_info[:username], db_info[:database] = 'travis', 'test'
-else
-  db_info[:username], db_info[:password], db_info[:database] = 'canto', DB_PASSWD, ENV['RACK_ENV']
-end
 
 RSpec.configure do |config|
   config.include JsonSpec::Helpers
