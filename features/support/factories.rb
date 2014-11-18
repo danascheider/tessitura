85deb83ce20b27dc63da87c64b57b767a21545d0
Dana Scheider <dana.scheider@gmail.com>
2014-11-18 10:44:59 -0800

    Add user with complete and incomplete tasks factory

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -64,5 +64,15 @@ FactoryGirl.define do
         create_list(:task_list_with_tasks, evaluator.lists_count, user: user)
       end
     end
+
+    factory :user_with_complete_and_incomplete_tasks do 
+      transient do 
+        list_count 2
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:task_list_with_complete_and_incomplete_tasks, evaluator.list_count, user: user)
+      end
+    end
   end
 end
