9cba476481df2c5823d4e3dcafa4a9fb5e2fb012
Dana Scheider <dana.scheider@gmail.com>
2014-11-19 10:03:02 -0800

    Merge master, delete unused helper method

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -1,9 +1,3 @@
-def decode_form_data(data)
-  form_data = URI::decode_www_form(data).flatten
-  hash = Hash[*form_data]
-  hash
-end
-
 def authorize_with(user)
   authorize user.username, user.password
 end
