8f9edba918e7408e5e14f2b73a0b5e49794a5c78
Dana Scheider <dana.scheider@gmail.com>
2014-11-25 13:26:39 -0800

    Return hash from sanitize_attributes

--- helpers/general_helper_methods.rb
+++ helpers/general_helper_methods.rb
@@ -12,6 +12,7 @@ module Sinatra
     def sanitize_attributes!(hash)
       bad_keys = [:id, :created_at, :updated_at, :owner_id]
       hash.reject! {|k,v| k.in?(bad_keys) }
+      hash
     end
   end
 end
