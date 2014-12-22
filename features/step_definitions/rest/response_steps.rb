68fcceca33c96eaa36731bbacbba4831d56831dc
Dana Scheider <dana.scheider@gmail.com>
2014-12-22 13:21:52 -0800

    Tweak step definitions to fix Travis build

--- features/step_definitions/rest/response_steps.rb
+++ features/step_definitions/rest/response_steps.rb
@@ -6,7 +6,7 @@ Then(/^the JSON response should include the (\d+)(?:[a-z]{2}) user's incomplete 
   expect(last_response.body).to eql User[id].tasks.where_not(:status, 'Complete').to_json
 end
 
-Then(/^the JSON response should include all the (.*)s$/) do |model|
+Then(/^the JSON response should include all the (\w+)s$/) do |model|
   expect(last_response.body).to eql eval(model.capitalize).all.to_json
 end
 
