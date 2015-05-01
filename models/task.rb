ec9e7553969ea6c2d91f9791d3d03152cebd2e04
Dana Scheider <dana.scheider@gmail.com>
2015-05-01 11:06:33 -0700

    Add more docs

--- models/task.rb
+++ models/task.rb
@@ -16,7 +16,7 @@
 class Task < Sequel::Model
   many_to_one :task_list
 
-  # The `dirty` plugin makes old attribute values available to hooks
+  # The ++dirty++ plugin makes old attribute values available to hooks
 
   self.plugin :dirty
 
@@ -51,7 +51,7 @@ class Task < Sequel::Model
     super
   end
 
-  # When a task has been saved, either on create or on update, the `after_save`
+  # When a task has been saved, either on create or on update, the ++after_save++
   # hook adjusts the positions of the other tasks belonging to the same user 
   # such that there are no gaps or duplicate indices on the list
 
@@ -69,12 +69,12 @@ class Task < Sequel::Model
     Task.where('position > ?', position).each {|t| t.this.update(position: t.position - 1 )}
   end
 
-  # The `before_validation` hook assigns automatic values before a task is 
+  # The ++before_validation++ hook assigns automatic values before a task is 
   # validated on creation or update:
   # * Ensures that a task list has been specified
-  # * Assigns the `:owner_id` attribute based on the owner of the specified task list
-  # * Sets `:status` to a default value of 'New' unless a valid status is given
-  # * Sets `:priority` to a default value of 'Normal' unless a valid priority level
+  # * Assigns the ++:owner_id++ attribute based on the owner of the specified task list
+  # * Sets ++:status++ to a default value of 'New' unless a valid status is given
+  # * Sets ++:priority++ to a default value of 'Normal' unless a valid priority level
   #   is given
 
   def before_validation
@@ -85,60 +85,65 @@ class Task < Sequel::Model
     self.backlog = nil if self.status === 'Complete'
   end
 
-  # The `Task.complete` scope returns all tasks whose status is 'Complete'
+  # The ++Task.complete++ scope returns all tasks whose status is 'Complete'
 
   def self.complete
     Task.where(status: 'Complete')
   end
 
-  # The `Task.fresh` scope includes all tasks that are neither complete
+  # The ++Task.fresh++ scope includes all tasks that are neither complete
   # nor backlogged
 
   def self.fresh
     Task.exclude(status: 'Complete').exclude(backlog: true)
   end
 
-  # The `Task.incomplete` scope includes all tasks with a status other 
+  # The ++Task.incomplete++ scope includes all tasks with a status other 
   # than 'Complete'. 
 
   def self.incomplete
     Task.exclude(status: 'Complete')
   end
 
+  # The ++::highest_position++ method finds the highest position in the list
+  # of tasks belonging to a given user, identified by ++owner_id++, and 
+  # satisfying an optional ++scope++. It returns the highest position as an
+  # integer.
+
   def self.highest_position(owner_id, scope=:all)
     Task.send(scope).where(owner_id: owner_id).map(&:position).max
   end
 
-  # The `Task.stale` scope includes all tasks that are either complete or
+  # The ++Task.stale++ scope includes all tasks that are either complete or
   # backlogged.
 
   def self.stale
     Task.where('status=? or backlog=?', 'Complete', true)
   end
 
-  # The `#complete?` method returns true if the task's status attribute is 
+  # The ++#complete?++ method returns true if the task's status attribute is 
   # set to 'Complete' and false otherwise.
 
   def complete?
     !incomplete?
   end
 
-  # The `#fresh?` method returns true if the task is not backlogged and its
+  # The ++#fresh?++ method returns true if the task is not backlogged and its
   # status is anything other than 'Complete'. If a task is backlogged or 
-  # complete, `#fresh?` returns false.
+  # complete, ++#fresh?++ returns false.
 
   def fresh?
     incomplete? && !backlog
   end
 
-  # The `#incomplete?` method returns true if the task's status has any value
+  # The ++#incomplete?++ method returns true if the task's status has any value
   # other than 'Complete'. If the task is complete, it returns false.
 
   def incomplete?
     status != 'Complete'
   end
 
-  # The `#to_hash` or `#to_h` method returns a hash of all of the task's 
+  # The ++#to_hash++ or ++#to_h++ method returns a hash of all of the task's 
   # non-empty attributes. Keys for blank attributes are removed from the
   # hash, so not all columns will necessarily be represented in the hash.
 
@@ -148,10 +153,10 @@ class Task < Sequel::Model
 
   alias_method :to_h, :to_hash
 
-  # Overwrites the default `#to_json` method to return a JSON object based
+  # Overwrites the default ++#to_json++ method to return a JSON object based
   # on the task's attribute hash
   #
-  # NOTE: The definition of `#to_json` has to include the optional `opts`
+  # NOTE: The definition of ++#to_json++ has to include the optional ++opts++
   #       arg, because in some of the tests, a JSON::Ext::Generator::State
   #       object is passed to the method. I am not sure why this happens,
   #       but including the optional arg makes it work as expected.
@@ -160,8 +165,8 @@ class Task < Sequel::Model
     to_h.to_json
   end
 
-  # The `user` method returns the user who ultimately owns the task. Since
-  # users own tasks only through the `:task_lists` table, this provides
+  # The ++user++ method returns the user who ultimately owns the task. Since
+  # users own tasks only through the ++:task_lists++ table, this provides
   # a direct reference to the user object that owns the list the given
   # task is on.
 
@@ -179,9 +184,9 @@ class Task < Sequel::Model
 
   private
 
-    # The Task.update_positions method uses `task.this` to carry out the 
+    # The Task.update_positions method uses ++task.this++ to carry out the 
     # #update action on a dataset consisting of the task. This is done to 
-    # skip the `before_update`/`before_save` hook calling update_positions,
+    # skip the ++before_update++/++before_save++ hook calling update_positions,
     # which would lead to infinite recursion.
 
     def self.adjust_positions(changed)
@@ -192,7 +197,7 @@ class Task < Sequel::Model
       return true if values === []
 
       # Tasks iterate through tasks, updating the positions. Note the use
-      # of `t.this.update(args)` - this carries out the `update` action on
+      # of ++t.this.update(args)++ - this carries out the ++update++ action on
       # a Sequel::Dataset representation of the task model, thus bypassing
       # any hooks otherwise triggered by an update.
 
@@ -204,9 +209,9 @@ class Task < Sequel::Model
     def self.return_values(positions) 
       dup, gap = Task.get_dup_and_gap(positions)[0], Task.get_dup_and_gap(positions)[1]
 
-      # No `dup` and no `gap` indicates no positions have been changed.
-      # (A `gap` with no `dup` indicates a task was destroyed, in which case
-      # that will be taken care of in the `after_destroy` hook.)
+      # No ++dup++ and no ++gap++ indicates no positions have been changed.
+      # (A ++gap++ with no ++dup++ indicates a task was destroyed, in which case
+      # that will be taken care of in the ++after_destroy++ hook.)
 
       return [] unless dup
 
@@ -223,7 +228,7 @@ class Task < Sequel::Model
       return [dup, gap].sort << (gap < dup ? -1 : 1)
     end
 
-    # The `Task.get_dup_and_gap` method takes a sorted list of indices as 
+    # The ++Task.get_dup_and_gap++ method takes a sorted list of indices as 
     # a parameter and returns the value of any duplicates, as well as any
     # indices that are missing between the lowest and highest indices on 
     # the list.
@@ -237,7 +242,7 @@ class Task < Sequel::Model
       #      task was created at or moved to
       #   2. A gap will appear at the position the task was removed from
       #
-      # `dup` and `gap` represent the duplicate position number and the 
+      # ++dup++ and ++gap++ represent the duplicate position number and the 
       # missing position number, respectively
       
       dup = positions.find {|number| positions.count(number) > 1 }
