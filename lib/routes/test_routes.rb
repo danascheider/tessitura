258556d5109d13acee02f2424b850b77a9dfadd0
Dana Scheider <dana.scheider@gmail.com>
2015-05-05 14:52:14 -0700

    Try reformatting seeds to prevent errors

--- lib/routes/test_routes.rb
+++ lib/routes/test_routes.rb
@@ -21,7 +21,7 @@ module Sinatra
             client.query('TRUNCATE TABLE listings')
             client.query('SET FOREIGN_KEY_CHECKS = 1')
 
-            seeds = JSON.parse(File.read(File.expand_path('../../../db/seeds.json', __FILE__)))
+            seeds = JSON.parse(File.read(File.expand_path('../../../db/seeds.json', __FILE__)), symbolize_names: true)
             seeds.to_h
             # user = User.try_rescue(:create, seeds[:user])
 
