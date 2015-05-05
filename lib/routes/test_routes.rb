58e1a52ca8879dea5744256a3dde2fcdbb9898ce
Dana Scheider <dana.scheider@gmail.com>
2015-05-05 15:20:26 -0700

    Try reformatting seeds to prevent errors

--- lib/routes/test_routes.rb
+++ lib/routes/test_routes.rb
@@ -28,7 +28,7 @@ module Sinatra
 
             user = User.create(user_attributes)
 
-            File.read(File.expand_path('../../../db/seeds.json', __FILE__))
+            tasks.first
           end
         end
       end
