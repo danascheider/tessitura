71c68cca20711a9110bfd43d73dc427f51bb9cba
Dana Scheider <dana.scheider@gmail.com>
2015-05-05 15:00:26 -0700

    Try reformatting seeds to prevent errors

--- lib/routes/test_routes.rb
+++ lib/routes/test_routes.rb
@@ -22,14 +22,16 @@ module Sinatra
             client.query('SET FOREIGN_KEY_CHECKS = 1')
 
             seeds = JSON.parse(File.read(File.expand_path('../../../db/seeds.json', __FILE__)), symbolize_names: true)
-            seeds.to_h.to_h
 
-            # user = User.try_rescue(:create, seeds[:user])
+            user_attributes = (seeds.select {|val| val[0] === :user }).first[1]
+            tasks = (seeds.select {|val| val[0] === :tasks }).first[1]
 
-            # seeds[:tasks].each do |data|
-            #   data[task_list_id: user.default_task_list]
-            #   Task.try_rescue(:create, data)
-            # end 
+            user = User.create(user_attributes)
+
+            tasks.each do |data|
+              data[:task_list_id] = user.default_task_list.id
+              Task.create(data)
+            end
           end
         end
       end
