2157ee9333efc03ce8073587f7b007e4e9ce8101
Dana Scheider <dana.scheider@gmail.com>
2015-05-05 15:04:40 -0700

    Try reformatting seeds to prevent errors

--- lib/routes/test_routes.rb
+++ lib/routes/test_routes.rb
@@ -23,9 +23,9 @@ module Sinatra
 
             seeds = JSON.parse(File.read(File.expand_path('../../../db/seeds.json', __FILE__)), symbolize_names: true)
 
-            user_attributes = (seeds.select {|val| val[0] === :user })[1]
-            tasks = (seeds.select {|val| val[0] === :tasks })[1]
-
+            user_attributes = (seeds.select {|val| val[0] === :user })
+            tasks = (seeds.select {|val| val[0] === :tasks })
+            
             {:user_attributes => user_attributes, :tasks => tasks}
             # user = User.create(user_attributes)
 
