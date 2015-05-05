e6576e0facdf37e18a599cecca73a512c0c9d854
Dana Scheider <dana.scheider@gmail.com>
2015-05-05 14:07:52 -0700

    Attempt to rescue error caused by failing Sequel Model stuff

--- lib/routes/test_routes.rb
+++ lib/routes/test_routes.rb
@@ -22,7 +22,7 @@ module Sinatra
             client.query('SET FOREIGN_KEY_CHECKS = 1')
 
             seeds = JSON.parse(File.read(File.expand_path('../../../db/seeds.json', __FILE__)))
-            seeds
+            seeds.to_h
             # user = User.try_rescue(:create, seeds[:user])
 
             # seeds[:tasks].each do |data|
