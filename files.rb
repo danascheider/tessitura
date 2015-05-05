38c9aecc8afe5112c3453ecb0e09729c9a1b20da
Dana Scheider <dana.scheider@gmail.com>
2015-05-05 11:28:15 -0700

    Add log files to package in hopes of fixing irritating problem

--- files.rb
+++ files.rb
@@ -7,11 +7,12 @@ module CantoPackage
     LIB_FILES = Dir.glob('./lib/**/*.rb').sort
     CONFIG_FILES = Dir.glob('./config/*').sort
     SCRIPT_FILES = Dir.glob('./script/**/*').sort
+    LOG_FILES = Dir.glob('./log/**/*').sort
     BIN_FILES = Dir.glob('./bin/**/*').sort
     TASK_FILES = ['Rakefile', Dir.glob('./tasks/**/*.rake')].flatten.sort
     TEST_FILES = [Dir.glob('./spec/**/*.rb'), Dir.glob('./features/**/*')].flatten.sort
     BASE_FILES = %w(files.rb Gemfile Vagrantfile config.ru canto.gemspec README.rdoc version.rb)
 
-    FILES = [LIB_FILES, CONFIG_FILES, SCRIPT_FILES, BIN_FILES, TASK_FILES, TEST_FILES, BASE_FILES].flatten
+    FILES = [LIB_FILES, CONFIG_FILES, SCRIPT_FILES, BIN_FILES, TASK_FILES, LOG_FILES, TEST_FILES, BASE_FILES].flatten
   end
 end
