776c50d9ad3d088448e0054d7d98eb0c9d08ed13
Dana Scheider <dana.scheider@gmail.com>
2015-05-06 10:34:48 -0700

    Add more docs

--- lib/models/listing.rb
+++ lib/models/listing.rb
@@ -1,12 +1,18 @@
 class Listing < Sequel::Model
   many_to_one :season
 
+  # The ++validate++ method verifies that a listing has a ++:title++ and a
+  # ++:season_id++, returning a ++Sequel::ValidationError++ if these 
+  # conditions aren't met. This method extends the ++validate++ method
+  # that is added to the ++Sequel::Model++ instance by the ++:validation_helpers++
+  # plugin.
+
   def validate
     super
     validates_presence [:title, :season_id]
   end
 
-  # The `#to_h` or `#to_hash` method returns a hash of the non-blank
+  # The ++#to_h++ or ++#to_hash++ method returns a hash of the non-blank
   # attributes of the listing.
 
   def to_hash
@@ -21,10 +27,10 @@ class Listing < Sequel::Model
 
   alias_method :to_h, :to_hash
 
-  # The `#to_json` method converts the output of `#to_hash` to JSON format, preventing
-  # inscrutable JSON objects like `"\"#<Listing:0x00000004b050c8>\""`.
+  # The ++#to_json++ method converts the output of ++#to_hash++ to JSON format, preventing
+  # inscrutable JSON objects like ++"\"#<Listing:0x00000004b050c8>\""++.
   #
-  # NOTE: The definition of `#to_json` has to include the optional `opts`
+  # NOTE: The definition of ++#to_json++ has to include the optional ++opts++
   #       arg, because in some of the tests, a JSON::Ext::Generator::State
   #       object is passed to the method. I am not sure why this happens,
   #       but including the optional arg makes it work as expected.
