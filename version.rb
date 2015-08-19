e97fea97d6c3937ffd65334d4bd791c6647c4f69
Dana Scheider <dana.scheider@gmail.com>
2015-08-19 12:42:55 -0700

    Update version to indicate this is for the test API only

--- version.rb
+++ version.rb
@@ -7,7 +7,7 @@ module TessituraPackage
     MAJOR = '0'
     MINOR = '3'
     PATCH = '0'
-    PRE   = 'alpha1'
+    PRE   = 'alpha1.test'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].join('.').chomp('.')
   end
