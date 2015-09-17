0433fbddf2def2a04c992e4593e448b5ccc8238e
Dana Scheider <dana.scheider@gmail.com>
2015-09-17 10:50:56 -0700

    Round out Geofilter route as a stopgap while I learn about Elasticsearch

--- lib/routes/geofilter_routes.rb
+++ lib/routes/geofilter_routes.rb
@@ -4,8 +4,12 @@ module Sinatra
       module GeoFilterRoutes
         def self.registered(app) 
           app.post '/geofilter' do 
-            filter = GeoFilter.new(Organization, request_body)
-            filter.filter!.to_json
+            if request_body[:state] || request_body[:zip]
+              filter = GeoFilter.new(Organization, request_body)
+              filter.filter!.to_json
+            else
+              [422, "Error: Please include state or postal code."]
+            end
           end
         end
       end
